37b020984bce905aaf587a3693cd5ac4d4efaca5
Len Bucchino <len@bitfly.org>
2022-12-26 21:56:27 -0600

    tweaks to PlayerCooldowns

--- src/main/java/com/example/testmod/capabilities/mana/data/PlayerCooldowns.java
+++ src/main/java/com/example/testmod/capabilities/mana/data/PlayerCooldowns.java
@@ -3,52 +3,60 @@ package com.example.testmod.capabilities.mana.data;
 import com.example.testmod.spells.SpellType;
 import com.google.common.collect.Maps;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerCooldowns {
 
     //spell type and for how many more ticks it will be on cooldown
-    private final Map<SpellType,CooldownInstance> spellCooldowns = Maps.newHashMap();
+    private final Map<SpellType, CooldownInstance> spellCooldowns = Maps.newHashMap();
 
-    public void tick(){
-        spellCooldowns.forEach((spell,cooldown)->{
+    public void tick() {
+        spellCooldowns.forEach((spell, cooldown) -> {
             boolean cooldownOver = decrementCooldown(cooldown);
-            if(cooldownOver)
+            if (cooldownOver)
                 spellCooldowns.remove(spell);
         });
     }
-    public void addCooldown(SpellType spell, int duration){
-        if(!spellCooldowns.containsKey(spell))
-            spellCooldowns.put(spell,new CooldownInstance(duration));
+
+    public void addCooldown(SpellType spell, int duration) {
+        if (!spellCooldowns.containsKey(spell))
+            spellCooldowns.put(spell, new CooldownInstance(duration));
     }
-    public boolean isOnCooldown(SpellType spell){
+
+    public boolean isOnCooldown(SpellType spell) {
         return spellCooldowns.containsKey(spell);
     }
-    public float getCooldownPercent(SpellType spell){
-        if(!spellCooldowns.containsKey(spell))
-            return 0f;
-        return spellCooldowns.get(spell).getCooldownPercent();
+
+    public float getCooldownPercent(SpellType spell) {
+        return spellCooldowns.getOrDefault(spell, new CooldownInstance(0)).getCooldownPercent();
     }
-    private boolean decrementCooldown(CooldownInstance c){
+
+    private boolean decrementCooldown(CooldownInstance c) {
         c.decrement();
-        return c.getCooldown()<=0;
+        return c.getCooldown() <= 0;
     }
-    class CooldownInstance{
-        private int cooldownLeft;
-        private int totalCooldown;
-        public CooldownInstance(int duration){
-            cooldownLeft = duration;
-            totalCooldown = duration;
+
+    class CooldownInstance {
+        private int cooldownRemaining;
+        private final int spellCooldown;
+
+        public CooldownInstance(int duration) {
+            cooldownRemaining = duration;
+            spellCooldown = duration;
         }
-        public void decrement(){
-            cooldownLeft--;
+
+        public void decrement() {
+            cooldownRemaining--;
         }
-        public int getCooldown(){
-            return cooldownLeft;
+
+        public int getCooldown() {
+            return cooldownRemaining;
         }
-        public float getCooldownPercent(){
-            return cooldownLeft / (float)totalCooldown;
+
+        public float getCooldownPercent() {
+            if (cooldownRemaining == 0) return 0;
+
+            return cooldownRemaining / (float) spellCooldown;
         }
     }
 }
