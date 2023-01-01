a4489c25de20736e6d22442a7d47add73445d51e
Christian <cmb@bitfly.org>
2023-01-01 17:03:23 -0600

    Long Cast Cancels on Damage Taken

--- src/main/java/com/example/testmod/player/ClientPlayerEvents.java
+++ src/main/java/com/example/testmod/player/ClientPlayerEvents.java
@@ -10,11 +10,14 @@ import com.example.testmod.setup.Messages;
 import com.example.testmod.spells.CastType;
 import com.example.testmod.spells.SpellType;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
@@ -26,6 +29,9 @@ import org.apache.logging.log4j.core.jmx.Server;
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientPlayerEvents {
 
+    //
+    //  Cancel Cast if we Switch off Spellbook
+    //
     public static void onLivingEquipmentChangeEvent(LivingEquipmentChangeEvent event) {
         //THIS EVENT IS SERVER SIDE ONLY
         if (event.getEntityLiving() == null) {
@@ -43,6 +49,9 @@ public class ClientPlayerEvents {
         }
     }
 
+    //
+    //  Cancel Cast if we Open a Container
+    //
     @SubscribeEvent
     public static void onPlayerOpenContainer(PlayerContainerEvent.Open event) {
         if (event.getPlayer().level.isClientSide)
@@ -53,6 +62,9 @@ public class ClientPlayerEvents {
 
     }
 
+    //
+    //  Cancel Cast if we Interact with an entity (horse, villager, minecart, etc)
+    //
     @SubscribeEvent
     public static void onPlayerRightClickEntity(PlayerInteractEvent.EntityInteract event) {
         if (event.getPlayer().level.isClientSide)
@@ -62,6 +74,42 @@ public class ClientPlayerEvents {
             Messages.sendToServer(new PacketCancelCast(false));
     }
 
+    //
+    //  Cancel Long Cast if we take non-dot damage
+    //
+    @SubscribeEvent
+    public static void onPlayerTakeDamage(LivingDamageEvent event) {
+        //THIS EVENT IS SERVER SIDE ONLY
+        if (event.getEntityLiving() == null) {
+            return;
+        }
+
+        /*
+        ON_FIRE is lingering fire ticks
+        IN_FIRE will still cancel cast
+
+        Poison (id 19) is magic damage
+        Wither (id 20) is wither damage
+        Poison is not the only source of magic damage (wither is)
+
+        We shall make magic damage interrupt cast, so poison will cancel.
+         */
+        var cap = event.getEntityLiving().getCapability(PlayerMagicProvider.PLAYER_MAGIC);
+        if (cap.isPresent()) {
+            var playerMagicData = cap.resolve().get();
+
+            if (playerMagicData.isCasting() &&
+                    event.getSource() != DamageSource.ON_FIRE &&
+                    event.getSource() != DamageSource.WITHER) {
+                TestMod.LOGGER.info("onPlayerTakeDamage: Cancel Cast");
+                Messages.sendToServer(new PacketCancelCast(false));
+            }
+        }
+    }
+
+    //
+    //  Handle (Client Side) cast duration
+    //
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if (event.side.isClient() && event.phase == TickEvent.Phase.END) {
             ClientMagicData.getCooldowns().tick(1);
