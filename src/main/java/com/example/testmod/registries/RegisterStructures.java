ab56cfa7d69c5974526c2c7857a5fa793f60846c
Len Bucchino <len@bitfly.org>
2023-01-10 18:54:36 -0600

    Structure gen fixed for 1.19.2

---
+++ src/main/java/com/example/testmod/registries/RegisterStructures.java
@@ -0,0 +1,10 @@
+package com.example.testmod.registries;
+
+//public class RegisterStructures {
+//    private static final DeferredRegister<StructureType<?>> STRUCTURES = DeferredRegister.create(Registry.STRUCTURE_TYPE_REGISTRY, TestMod.MODID);
+//    public static final RegistryObject<StructureType<?>> PORTAL = STRUCTURES.register("tower", () -> typeConvert(TowerStructure.CODEC));
+//
+//    private static <S extends Structure> StructureType<S> typeConvert(Codec<S> codec) {
+//        return () -> codec;
+//    }
+//}
