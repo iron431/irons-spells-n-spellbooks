a495e6288e850b5b91be31e27598ede9049f0852
Len Bucchino <len@bitfly.org>
2023-01-16 20:54:26 -0600

    Entity mount event cancel cast

--- src/main/java/com/example/testmod/player/ServerPlayerEvents.java
+++ src/main/java/com/example/testmod/player/ServerPlayerEvents.java
@@ -5,24 +5,16 @@ import com.example.testmod.capabilities.magic.PlayerMagicData;
 import com.example.testmod.item.Scroll;
 import com.example.testmod.item.SpellBook;
 import com.example.testmod.network.PacketCancelCast;
-import com.example.testmod.registries.ItemRegistry;
 import com.example.testmod.spells.CastType;
 import com.example.testmod.spells.SpellType;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
-import net.minecraft.world.item.trading.Merchant;
+import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import static net.minecraft.world.item.Items.GLASS_BOTTLE;
 
 @Mod.EventBusSubscriber()
 public class ServerPlayerEvents {
@@ -78,32 +70,14 @@ public class ServerPlayerEvents {
     }
 
     @SubscribeEvent
-    public static void onPlayerRightClickEntity(PlayerInteractEvent.EntityInteract event) {
-
+    public static void onEntityMountEvent(EntityMountEvent event) {
         if (event.getEntity().level.isClientSide) {
             return;
         }
-        TestMod.LOGGER.debug("onPlayerRightClickEntity {} {}", event.getEntity().getName().getString(), event.getTarget().getName().getString());
 
         if (event.getEntity() instanceof ServerPlayer serverPlayer) {
             var playerMagicData = PlayerMagicData.getPlayerMagicData(serverPlayer);
-            var useHand = serverPlayer.getUsedItemHand();
-            if (serverPlayer.getItemInHand(useHand).is(GLASS_BOTTLE)) {
-                //TestMod.LOGGER.debug("onPlayerRightClickEntity: Glass Bottle");
-                if (event.getTarget() instanceof Creeper creeper) {
-                    //TestMod.LOGGER.debug("onPlayerRightClickEntity: Creeper");
-                    if (creeper.isPowered()) {
-                        //TestMod.LOGGER.debug("onPlayerRightClickEntity: Charged");
-                        //((ClientLevel) player.level).playSound(player, new BlockPos(player.getX(), player.getY(), player.getZ()), SoundEvents.BOTTLE_FILL_DRAGONBREATH, SoundSource.NEUTRAL, 1.0F, 1.0F);
-                        serverPlayer.getItemInHand(useHand).use(serverPlayer.level,serverPlayer,useHand);
-                        ItemStack bottleStack = serverPlayer.getItemInHand(serverPlayer.getUsedItemHand());
-                        ItemUtils.createFilledResult(bottleStack, serverPlayer, new ItemStack(ItemRegistry.LIGHTNING_BOTTLE.get()));
-                        creeper.ignite();
-                        event.setCancellationResult(InteractionResult.SUCCESS);
-                        event.setCanceled(true);
-                    }
-                }
-            } else if (event.getTarget() instanceof Merchant && playerMagicData.isCasting()) {
+            if (playerMagicData.isCasting()) {
                 serverSideCancelCast(serverPlayer, playerMagicData);
             }
         }
