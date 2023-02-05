6500138756d8a62e989a5cb52c326e6e77629861
Len Bucchino <len@bitfly.org>
2023-02-05 14:24:01 -0600

    wisp sounds

--- src/main/java/com/example/testmod/registries/SoundRegistry.java
+++ src/main/java/com/example/testmod/registries/SoundRegistry.java
@@ -18,6 +18,7 @@ public class SoundRegistry {
     public static RegistryObject<SoundEvent> FORCE_IMPACT = registerSoundEvent("force_impact");
     public static RegistryObject<SoundEvent> MAGIC_SPELL_REVERSE_3 = registerSoundEvent("magic_spell_reverse_3");
     public static RegistryObject<SoundEvent> ARIAL_SUMMONING_5_CUSTOM_1 = registerSoundEvent("arial_summoning_5_custom_1");
+    public static RegistryObject<SoundEvent> DARK_MAGIC_BUFF_03_CUSTOM_1 = registerSoundEvent("dark_magic_buff_03_custom_1");
 
     private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
         return SOUND_EVENTS.register(name, () -> new SoundEvent(new ResourceLocation(TestMod.MODID, name)));
