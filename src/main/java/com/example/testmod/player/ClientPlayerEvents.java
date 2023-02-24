160f6f7a6efe93a82acbb48e0f8c018e95b481c3
Christian <cmb@bitfly.org>
2023-02-24 16:57:15 -0600

    Client Particle Event Refactor

--- src/main/java/com/example/testmod/player/ClientPlayerEvents.java
+++ src/main/java/com/example/testmod/player/ClientPlayerEvents.java
@@ -1,11 +1,17 @@
 package com.example.testmod.player;
 
 import com.example.testmod.effect.AbyssalShroudEffect;
+import com.example.testmod.entity.AbstractSpellCastingMob;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientPlayerEvents {
@@ -15,19 +21,24 @@ public class ClientPlayerEvents {
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if (event.side.isClient() && event.phase == TickEvent.Phase.END && event.player == Minecraft.getInstance().player) {
+            var level = Minecraft.getInstance().level;
+
             ClientMagicData.getCooldowns().tick(1);
             if (ClientMagicData.getCastDuration() > 0) {
                 ClientMagicData.handleCastDuration();
             }
 
-            //TODO: what about spellcasting mobs?
-            var level = Minecraft.getInstance().level;
-            level.players().forEach((player) -> {
-                var spellData = ClientMagicData.getSyncedSpellData(player);
-                if (spellData.hasAbyssalShroud()) {
-                    AbyssalShroudEffect.ambientParticles(level, player);
-                }
-            });
+            if(level!=null){
+                List<Entity> spellcasters = level.getEntities((Entity) null, event.player.getBoundingBox().inflate(64), (mob) -> mob instanceof Player || mob instanceof AbstractSpellCastingMob);
+                spellcasters.forEach((entity) -> {
+                    LivingEntity livingEntity = (LivingEntity) entity;
+                    var spellData = ClientMagicData.getSyncedSpellData(livingEntity);
+                    if (spellData.hasAbyssalShroud()) {
+                        AbyssalShroudEffect.ambientParticles(level, livingEntity);
+                    }
+                });
+            }
+
         }
     }
 }
