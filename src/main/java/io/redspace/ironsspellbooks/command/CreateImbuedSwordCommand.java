519e8afb9714845889bc982b1d29f52d4cb0bd34
Len Bucchino <len@bitfly.org>
2023-04-01 19:17:36 -0500

    All create commands should honor max spell level

--- src/main/java/io/redspace/ironsspellbooks/command/CreateImbuedSwordCommand.java
+++ src/main/java/io/redspace/ironsspellbooks/command/CreateImbuedSwordCommand.java
@@ -1,12 +1,12 @@
 package io.redspace.ironsspellbooks.command;
 
-import io.redspace.ironsspellbooks.capabilities.spell.SpellData;
-import io.redspace.ironsspellbooks.spells.SpellType;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
+import io.redspace.ironsspellbooks.capabilities.spell.SpellData;
+import io.redspace.ironsspellbooks.spells.SpellType;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 public class CreateImbuedSwordCommand {
 
     private static final SimpleCommandExceptionType ERROR_FAILED = new SimpleCommandExceptionType(Component.translatable("commands.irons_spellbooks.create_imbued_sword.failed"));
+    private static final SimpleCommandExceptionType ERROR_FAILED_MAX_LEVEL = new SimpleCommandExceptionType(Component.translatable("commands.irons_spellbooks.create_imbued_sword.failed_max_level"));
 
     private static final SuggestionProvider<CommandSourceStack> SWORD_SUGGESTIONS = (p_180253_, p_180254_) -> {
         var resources = Registry.ITEM.stream().filter((k) -> k instanceof SwordItem).map(Registry.ITEM::getKey).collect(Collectors.toSet());
@@ -36,12 +37,16 @@ public class CreateImbuedSwordCommand {
             return commandSourceStack.hasPermission(2);
         }).then(Commands.argument("item", ItemArgument.item(context)).suggests(SWORD_SUGGESTIONS)
                 .then(Commands.argument("spellType", EnumArgument.enumArgument(SpellType.class))
-                        .then(Commands.argument("level", IntegerArgumentType.integer(0, 10)).executes((ctx) -> {
+                        .then(Commands.argument("level", IntegerArgumentType.integer(1)).executes((ctx) -> {
                             return createImbuedSword(ctx.getSource(), ctx.getArgument("item", ItemInput.class), ctx.getArgument("spellType", SpellType.class), IntegerArgumentType.getInteger(ctx, "level"));
                         })))));
     }
 
     private static int createImbuedSword(CommandSourceStack source, ItemInput itemInput, SpellType spellType, int spellLevel) throws CommandSyntaxException {
+        if (spellLevel > spellType.getMaxLevel()) {
+            throw new SimpleCommandExceptionType(Component.translatable("commands.irons_spellbooks.create_spell.failed_max_level", spellType, spellType.getMaxLevel())).create();
+        }
+
         var serverPlayer = source.getPlayer();
         if (serverPlayer != null) {
             ItemStack itemstack = new ItemStack(itemInput.getItem());
--- src/main/java/io/redspace/ironsspellbooks/command/CreateScrollCommand.java
+++ src/main/java/io/redspace/ironsspellbooks/command/CreateScrollCommand.java
@@ -1,12 +1,12 @@
 package io.redspace.ironsspellbooks.command;
 
-import io.redspace.ironsspellbooks.capabilities.spell.SpellData;
-import io.redspace.ironsspellbooks.registries.ItemRegistry;
-import io.redspace.ironsspellbooks.spells.SpellType;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import io.redspace.ironsspellbooks.capabilities.spell.SpellData;
+import io.redspace.ironsspellbooks.registries.ItemRegistry;
+import io.redspace.ironsspellbooks.spells.SpellType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
@@ -26,12 +26,16 @@ public class CreateScrollCommand {
     public static void register(CommandDispatcher<CommandSourceStack> pDispatcher) {
         pDispatcher.register(Commands.literal("createScroll").requires((p_138819_) -> {
             return p_138819_.hasPermission(2);
-        }).then(Commands.argument("spellType", EnumArgument.enumArgument(SpellType.class)).then(Commands.argument("level", IntegerArgumentType.integer(0, 10)).executes((commandContext) -> {
+        }).then(Commands.argument("spellType", EnumArgument.enumArgument(SpellType.class)).then(Commands.argument("level", IntegerArgumentType.integer(1)).executes((commandContext) -> {
             return createScroll(commandContext.getSource(), commandContext.getArgument("spellType", SpellType.class), IntegerArgumentType.getInteger(commandContext, "level"));
         }))));
     }
 
     private static int createScroll(CommandSourceStack source, SpellType spellType, int spellLevel) throws CommandSyntaxException {
+        if (spellLevel > spellType.getMaxLevel()) {
+            throw new SimpleCommandExceptionType(Component.translatable("commands.irons_spellbooks.create_spell.failed_max_level", spellType, spellType.getMaxLevel())).create();
+        }
+
         var serverPlayer = source.getPlayer();
         if (serverPlayer != null) {
             ItemStack itemstack = new ItemStack(ItemRegistry.SCROLL.get());
