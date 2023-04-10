6a4772bc4c3be01cdf339eacddf5d83713fdf34b
Christian <cmb@bitfly.org>
2023-04-10 15:26:31 -0500

    Villager Cage Spawns

--- src/main/java/io/redspace/ironsspellbooks/entity/mobs/keeper/KeeperEntity.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/mobs/keeper/KeeperEntity.java
@@ -81,7 +81,6 @@ public class KeeperEntity extends AbstractSpellCastingMob implements Enemy {
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.goalSelector.addGoal(4, new KeeperAnimatedWarlockAttackGoal(this, 1f, 10, 30, 3.5f));
         this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
 
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
