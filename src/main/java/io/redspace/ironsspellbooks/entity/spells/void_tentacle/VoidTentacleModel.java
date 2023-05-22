0e2712fd1ecd51b5e3927bdbdba07651c14685b7
Christian <cmb@bitfly.org>
2023-05-22 01:15:29 -0500

    Scrolling Texture

--- src/main/java/io/redspace/ironsspellbooks/entity/spells/void_tentacle/VoidTentacleModel.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/spells/void_tentacle/VoidTentacleModel.java
@@ -6,7 +6,7 @@ import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class VoidTentacleModel extends AnimatedGeoModel<VoidTentacle> {
     public static final ResourceLocation modelResource = new ResourceLocation(IronsSpellbooks.MODID, "geo/void_tentacle.geo.json");
-    public static final ResourceLocation textureResource = IronsSpellbooks.id("textures/entity/void_tentacle.png");
+    public static final ResourceLocation textureResource = IronsSpellbooks.id("textures/entity/void_tentacle/void_tentacle.png");
     public static final ResourceLocation animationResource = new ResourceLocation(IronsSpellbooks.MODID, "animations/void_tentacle_animations.json");
 
     @Override
--- src/main/java/io/redspace/ironsspellbooks/render/SpellRenderingHelper.java
+++ src/main/java/io/redspace/ironsspellbooks/render/SpellRenderingHelper.java
@@ -18,24 +18,32 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class SpellRenderingHelper {
-    private static ResourceLocation SOLID = IronsSpellbooks.id("textures/entity/electric_beams/solid.png");
+    private static ResourceLocation SOLID = IronsSpellbooks.id("textures/entity/ray/solid.png");
+    private static ResourceLocation BEACON = IronsSpellbooks.id("textures/entity/ray/beacon_beam.png");
+    private static ResourceLocation GLOW = IronsSpellbooks.id("textures/entity/ray/ribbon_glow.png");
 
     public static void renderRay(LivingEntity entity, PoseStack poseStack, MultiBufferSource bufferSource, int r, int g, int b, int a, float partialTicks) {
         poseStack.pushPose();
         poseStack.translate(0, entity.getEyeHeight() * .8f, 0);
         float f = Mth.rotlerp(entity.yRotO, entity.getYRot(), partialTicks);
         float f1 = Mth.lerp(partialTicks, entity.xRotO, entity.getXRot());
-        IronsSpellbooks.LOGGER.debug("Visual POS: yrot: {} yrotold: {} parital ticks: {} inbetween: {}", entity.getYRot(), entity.yRotO, partialTicks, f);
+        //IronsSpellbooks.LOGGER.debug("Visual POS: yrot: {} yrotold: {} parital ticks: {} inbetween: {}", entity.getYRot(), entity.yRotO, partialTicks, f);
         poseStack.mulPose(Vector3f.YP.rotationDegrees(-f));
         poseStack.mulPose(Vector3f.XP.rotationDegrees(f1));
         var pose = poseStack.last();
         Vec3 start = Vec3.ZERO;//caster.getEyePosition(partialTicks);
         Vec3 end = new Vec3(0, 0, 10);//Utils.raycastForEntity(entity.level, entity, 32, true).getLocation().subtract(entity.position());
         //IronsSpellbooks.LOGGER.debug("RenderRay: {} to {}", start, end);
-        VertexConsumer inner = bufferSource.getBuffer(RenderType.entityCutout(SOLID));
-        VertexConsumer outer = bufferSource.getBuffer(RenderType.entityTranslucentEmissive(SOLID));
-        drawHull(start, end, .125f, .125f, pose, inner, r, g, b, a);
-        drawHull(start, end, .25f, .25f, pose, outer, r / 2, g / 2, b / 2, a / 2);
+        VertexConsumer inner = bufferSource.getBuffer(RenderType.entityCutout(BEACON));
+        VertexConsumer outer = bufferSource.getBuffer(RenderType.entityTranslucentEmissive(BEACON));
+        for (int j = 1; j <= 10 / 1; j++) {
+            end = new Vec3(0, 0, j);
+            drawHull(start, end, .125f, .125f, pose, inner, r, g, b, a);
+            drawHull(start, end, .25f, .25f, pose, outer, r / 2, g / 2, b / 2, a / 2);
+            start = end;
+
+        }
+        i++;
         poseStack.popPose();
     }
 
@@ -50,18 +58,20 @@ public class SpellRenderingHelper {
         drawQuad(from.add(width * .5f, 0, 0), to.add(width * .5f, 0, 0), 0, height, pose, consumer, r, g, b, a);
     }
 
+    static int i;
     private static void drawQuad(Vec3 from, Vec3 to, float width, float height, PoseStack.Pose pose, VertexConsumer consumer, int r, int g, int b, int a) {
         Matrix4f poseMatrix = pose.pose();
         Matrix3f normalMatrix = pose.normal();
-
-        //to = new Vec3(1, 0, 10);
+        float f1 = -(i / 40f) % 40f;
+        float max = Mth.frac(f1 * 0.2F - (float) Mth.floor(f1 * 0.1F));
+        float min = -1.0F + max;
         float halfWidth = width * .5f;
         float halfHeight = height * .5f;
-        //float height = (float) (Math.random() * .25f) + .25f;
-        consumer.vertex(poseMatrix, (float) from.x - halfWidth, (float) from.y - halfHeight, (float) from.z).color(r, g, b, a).uv(0f, 1f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
-        consumer.vertex(poseMatrix, (float) from.x + halfWidth, (float) from.y + halfHeight, (float) from.z).color(r, g, b, a).uv(1f, 1f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
-        consumer.vertex(poseMatrix, (float) to.x + halfWidth, (float) to.y + halfHeight, (float) to.z).color(r, g, b, a).uv(1f, 0f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
-        consumer.vertex(poseMatrix, (float) to.x - halfWidth, (float) to.y - halfHeight, (float) to.z).color(r, g, b, a).uv(0f, 0f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
+
+        consumer.vertex(poseMatrix, (float) from.x - halfWidth, (float) from.y - halfHeight, (float) from.z).color(r, g, b, a).uv(0f, min).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
+        consumer.vertex(poseMatrix, (float) from.x + halfWidth, (float) from.y + halfHeight, (float) from.z).color(r, g, b, a).uv(1f, min).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
+        consumer.vertex(poseMatrix, (float) to.x + halfWidth, (float) to.y + halfHeight, (float) to.z).color(r, g, b, a).uv(1f, max).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
+        consumer.vertex(poseMatrix, (float) to.x - halfWidth, (float) to.y - halfHeight, (float) to.z).color(r, g, b, a).uv(0f, max).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(240).normal(normalMatrix, 0f, 1f, 0f).endVertex();
 
     }
 }
