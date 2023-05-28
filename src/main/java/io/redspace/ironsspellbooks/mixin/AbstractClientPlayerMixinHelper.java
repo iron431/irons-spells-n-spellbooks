d0b89acba0ea61179c2d7984d1857aaff5c2580e
Len Bucchino <len@bitfly.org>
2023-05-28 13:08:39 -0500

    Fix playeranimator dependency issue

---
+++ src/main/java/io/redspace/ironsspellbooks/mixin/AbstractClientPlayerMixinHelper.java
@@ -0,0 +1,51 @@
+package io.redspace.ironsspellbooks.mixin;
+
+import dev.kosmx.playerAnim.api.layered.IAnimation;
+import dev.kosmx.playerAnim.api.layered.ModifierLayer;
+import dev.kosmx.playerAnim.api.layered.modifier.AdjustmentModifier;
+import dev.kosmx.playerAnim.api.layered.modifier.MirrorModifier;
+import dev.kosmx.playerAnim.core.util.Vec3f;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess;
+import io.redspace.ironsspellbooks.spells.AbstractSpell;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.Optional;
+
+public class AbstractClientPlayerMixinHelper {
+
+    public static void playerMixinInit(Player player) {
+        var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData((AbstractClientPlayer) player).get(AbstractSpell.ANIMATION_RESOURCE);
+        if (animation != null) {
+            animation.addModifierLast(new AdjustmentModifier((partName) -> {
+                float rotationX = 0;
+                float rotationY = 0;
+                float rotationZ = 0;
+                float offsetX = 0;
+                float offsetY = 0;
+                float offsetZ = 0;
+                var pitch = player.getXRot();
+                var yaw = player.yHeadRot - player.yBodyRot;
+                pitch = (float) Math.toRadians(pitch);
+                yaw = (float) Math.toRadians(yaw);
+                switch (partName) {
+                    case "rightArm", "leftArm" -> {
+                        rotationX = pitch;
+                        rotationY = yaw;
+                    }
+                    default -> {
+                        return Optional.empty();
+                    }
+                }
+                return Optional.of(new AdjustmentModifier.PartModifier(new Vec3f(rotationX, rotationY, rotationZ), new Vec3f(offsetX, offsetY, offsetZ)));
+            }));
+            animation.addModifierLast(new MirrorModifier() {
+                @Override
+                public boolean isEnabled() {
+                    return player.getUsedItemHand() == InteractionHand.OFF_HAND;
+                }
+            });
+        }
+    }
+}
