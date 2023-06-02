08e5a4a9087126da7ea413c350d9758c6cc518e4
Christian <cmb@bitfly.org>
2023-06-02 15:59:18 -0500

    SpellsOfSchool no longer returns disabled or NONE spells

--- src/main/java/io/redspace/ironsspellbooks/spells/SpellType.java
+++ src/main/java/io/redspace/ironsspellbooks/spells/SpellType.java
@@ -295,7 +295,7 @@ public enum SpellType {
 
 
     public static List<SpellType> getSpellsFromSchool(SchoolType school) {
-        return Arrays.stream(SpellType.values()).filter(spellType -> spellType.getSchoolType() == school).toList();
+        return Arrays.stream(SpellType.values()).filter(spellType -> spellType.getSchoolType() == school && spellType.isEnabled() && spellType != NONE_SPELL).toList();
 //        return spellsForSchool.getOrDefault(school, List.of(NONE_SPELL));
     }
 
