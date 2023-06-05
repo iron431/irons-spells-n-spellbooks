1ca64dcf477ee580bcefdbace8c309c442e735d5
Len Bucchino <len@bitfly.org>
2023-06-05 00:28:46 -0500

    disable logging

--- src/main/java/io/redspace/ironsspellbooks/util/Log.java
+++ src/main/java/io/redspace/ironsspellbooks/util/Log.java
@@ -2,5 +2,5 @@ package io.redspace.ironsspellbooks.util;
 
 public class Log {
     //Add more items here to control logging as needed for testing
-    public final static boolean SPELL_DEBUG = true;
+    public final static boolean SPELL_DEBUG = false;
 }
