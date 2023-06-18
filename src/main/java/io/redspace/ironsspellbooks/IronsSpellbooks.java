0b88914df2e0ed1d06b3c2557c97ab7fbbcb3221
Christian <cmb@bitfly.org>
2023-06-17 21:21:10 -0500

    Armor Pile Visual Fix

--- src/main/java/io/redspace/ironsspellbooks/IronsSpellbooks.java
+++ src/main/java/io/redspace/ironsspellbooks/IronsSpellbooks.java
@@ -88,9 +88,6 @@ public class IronsSpellbooks {
         MenuScreens.register(MenuRegistry.SCROLL_FORGE_MENU.get(), ScrollForgeScreen::new);
         MenuScreens.register(MenuRegistry.ARCANE_ANVIL_MENU.get(), ArcaneAnvilScreen::new);
 
-        ItemBlockRenderTypes.setRenderLayer(BlockRegistry.INSCRIPTION_TABLE_BLOCK.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(BlockRegistry.ARMOR_PILE_BLOCK.get(), RenderType.translucent());
-
 
     }
 
