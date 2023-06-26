eb31a4c4c98499e3aa9c9260ceec5b9d4df434ce
Len Bucchino <len@bitfly.org>
2023-06-25 20:56:14 -0500

    Plugin work

--- src/main/java/io/redspace/ironsspellbooks/plugin/PluginRegistration.java
+++ src/main/java/io/redspace/ironsspellbooks/plugin/PluginRegistration.java
@@ -1,14 +1,29 @@
 package io.redspace.ironsspellbooks.plugin;
 
 import io.redspace.ironsspellbooks.IronsSpellbooks;
+import io.redspace.ironsspellbooks.api.IIronsSpellbooksPlugin;
+
+import java.util.ArrayList;
+
 public class PluginRegistration {
     public static void LoadPlugins() {
+        var validPlugins = new ArrayList<IIronsSpellbooksPlugin>();
         PluginDiscovery.getPlugins().forEach(plugin -> {
             try {
                 IronsSpellbooks.LOGGER.debug("ISS Plugin: Loading: {}", plugin);
-                IronsSpellbooks.LOGGER.debug("ISS Plugin: {}", plugin.getPluginId());
+                plugin.registerSpells(null);//TODO: this needs real code
+                validPlugins.add(plugin);
             } catch (Exception e) {
-                IronsSpellbooks.LOGGER.error("ISS Plugin Error: Skipping plugin");
+                IronsSpellbooks.LOGGER.error("ISS Plugin Error: Skipping plugin {} {}", plugin, e);
+            }
+        });
+
+        validPlugins.forEach(validPlugin -> {
+            try {
+                IronsSpellbooks.LOGGER.debug("ISS Plugin: Finalizing: {}", validPlugin);
+                validPlugin.onAllSpellsRegistered();
+            } catch (Exception e) {
+                IronsSpellbooks.LOGGER.error("ISS Plugin Error: {} {}", validPlugin, e);
             }
         });
     }
