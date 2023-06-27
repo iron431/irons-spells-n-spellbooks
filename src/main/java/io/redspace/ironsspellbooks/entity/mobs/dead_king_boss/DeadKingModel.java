e9b05f36a9e9a2d6ba995bb2c4ecf59e40a73687
Christian <cmb@bitfly.org>
2023-06-27 16:29:49 -0500

    dead king model init

--- src/main/java/io/redspace/ironsspellbooks/entity/mobs/dead_king_boss/DeadKingModel.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/mobs/dead_king_boss/DeadKingModel.java
@@ -7,9 +7,12 @@ import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.Abstra
 import net.minecraft.resources.ResourceLocation;
 
 public class DeadKingModel extends AbstractSpellCastingMobModel {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king.png");
-    private static final ResourceLocation TEXTURE2 = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king_resting.png");
-    private static final ResourceLocation TEXTURE3 = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king_enraged.png");
+    //private static final ResourceLocation TEXTURE = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king.png");
+    //private static final ResourceLocation TEXTURE2 = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king_resting.png");
+    //private static final ResourceLocation TEXTURE3 = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king_enraged.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king_texture.png");
+    private static final ResourceLocation TEXTURE2 = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/corpse.png");
+    private static final ResourceLocation TEXTURE3 = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/dead_king/dead_king_phase2_texture.png");
     private static final ResourceLocation MODEL = new ResourceLocation(IronsSpellbooks.MODID, "geo/dead_king.geo.json");
 
     public DeadKingModel() {
