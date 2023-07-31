cc05c95e62565759b079fd683c3c23b1c57e287d
Christian <cmb@bitfly.org>
2023-07-30 20:03:42 -0500

    leg animations

--- src/main/java/io/redspace/ironsspellbooks/entity/mobs/keeper/KeeperModel.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/mobs/keeper/KeeperModel.java
@@ -3,7 +3,12 @@ package io.redspace.ironsspellbooks.entity.mobs.keeper;
 import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobModel;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.PartNames;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 
 public class KeeperModel extends AbstractSpellCastingMobModel {
     public static final ResourceLocation TEXTURE = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/keeper/keeper.png");
@@ -17,5 +22,53 @@ public class KeeperModel extends AbstractSpellCastingMobModel {
     @Override
     public ResourceLocation getModelResource(AbstractSpellCastingMob object) {
         return modelResource;
+    }
+
+    @Override
+    public void setCustomAnimations(AbstractSpellCastingMob entity, int instanceId, AnimationEvent animationEvent) {
+        super.setCustomAnimations(entity, instanceId, animationEvent);
+        if (Minecraft.getInstance().isPaused() || !entity.shouldBeExtraAnimated())
+            return;
+
+        float partialTick = animationEvent.getPartialTick();
+
+        IBone rightLeg = this.getAnimationProcessor().getBone(PartNames.RIGHT_LEG);
+        IBone leftLeg = this.getAnimationProcessor().getBone(PartNames.LEFT_LEG);
+
+        float pLimbSwingAmount = 0.0F;
+        float pLimbSwing = 0.0F;
+        if (entity.isAlive()) {
+            pLimbSwingAmount = Mth.lerp(partialTick, entity.animationSpeedOld, entity.animationSpeed);
+            pLimbSwing = entity.animationPosition - entity.animationSpeed * (1.0F - partialTick);
+            //pLimbSwingAmount *= .75f;
+            //pLimbSwing *= .75f;
+            if (pLimbSwingAmount > 1.0F) {
+                pLimbSwingAmount = 1.0F;
+            }
+        }
+//        rightLeg.setRotX(Mth.cos(pLimbSwing * 0.6662F) * 1.4F * pLimbSwingAmount / f);
+        if (!(entity.isPassenger() && entity.getVehicle().shouldRiderSit())) {
+            float strength = .75f;
+            updatePosition(rightLeg, 0, Mth.cos(pLimbSwing * 0.6662F) * 4  * strength * pLimbSwingAmount, -Mth.sin(pLimbSwing * 0.6662F) * 4 * pLimbSwingAmount);
+            updatePosition(leftLeg, 0, Mth.cos(pLimbSwing * 0.6662F - Mth.PI) * 4 * strength * pLimbSwingAmount, -Mth.sin(pLimbSwing * 0.6662F - Mth.PI) * 4 * pLimbSwingAmount);
+            if (!entity.isAnimating() || entity.shouldAlwaysAnimateLegs() && lastTick != entity.tickCount) {
+                legTween = Mth.lerp(.9f, 0, 1);
+                lastTick = entity.tickCount;
+            } else if (lastTick != entity.tickCount) {
+                legTween = Mth.lerp(.9f, 1, 0);
+                lastTick = entity.tickCount;
+            }
+            rightLeg.setRotationX(Mth.cos(pLimbSwing * 0.6662F) * 1.4F * pLimbSwingAmount * legTween * strength);
+            leftLeg.setRotationX(Mth.cos(pLimbSwing * 0.6662F + (float) Math.PI) * 1.4F * pLimbSwingAmount * legTween * strength);
+        }
+    }
+
+    private int lastTick;
+    private float legTween = 1f;
+
+    private static void updatePosition(IBone bone, float x, float y, float z) {
+        bone.setPositionX(x);
+        bone.setPositionY(y);
+        bone.setPositionZ(z);
     }
 }
