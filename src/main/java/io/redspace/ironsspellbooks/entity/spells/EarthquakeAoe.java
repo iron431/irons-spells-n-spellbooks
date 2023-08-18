61020a8429a130eac1e9e63a17dc05ff41793316
iron431 <34083081+iron431@users.noreply.github.com>
2023-08-18 17:02:02 -0500

    earthquake camera shake

--- src/main/java/io/redspace/ironsspellbooks/entity/spells/EarthquakeAoe.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/spells/EarthquakeAoe.java
@@ -1,5 +1,6 @@
 package io.redspace.ironsspellbooks.entity.spells;
 
+import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.api.util.Utils;
@@ -9,7 +10,6 @@ import io.redspace.ironsspellbooks.entity.mobs.AntiMagicSusceptible;
 import io.redspace.ironsspellbooks.registries.EntityRegistry;
 import io.redspace.ironsspellbooks.registries.SoundRegistry;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
@@ -20,14 +20,28 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ViewportEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Mod.EventBusSubscriber(Dist.CLIENT)
 public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
+    public static List<EarthquakeAoe> clientEarthquakeOrigins = new ArrayList<>();
 
     public EarthquakeAoe(EntityType<? extends Projectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
         this.reapplicationDelay = 25;
         this.setCircular();
+        if (this.level.isClientSide) {
+            clientEarthquakeOrigins.add(this);
+        }
     }
+
     public EarthquakeAoe(Level level) {
         this(EntityRegistry.EARTHQUAKE_AOE.get(), level);
     }
@@ -64,6 +78,7 @@ public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
     }
 
     int waveAnim = -1;
+
     @Override
     public void tick() {
         super.tick();
@@ -82,7 +97,7 @@ public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
             for (int i = 0; i < intensity; i++) {
                 Vec3 vec3 = this.position().add(uniformlyDistributedPointInRadius(radius));
                 BlockPos blockPos = new BlockPos(Utils.moveToRelativeGroundLevel(level, vec3, 4)).below();
-                createTremorBlock(blockPos);
+                createTremorBlock(blockPos, .1f + random.nextFloat() * .2f);
             }
             if (waveAnim >= 0) {
                 var circumference = waveAnim * 2 * 3.14f;
@@ -95,7 +110,7 @@ public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
                             waveAnim * Mth.sin(anglePerBlock * i)
                     );
                     BlockPos blockPos = new BlockPos(Utils.moveToRelativeGroundLevel(level, position().add(vec3), 4)).below();
-                    createTremorBlock(blockPos);
+                    createTremorBlock(blockPos, .1f + random.nextFloat() * .2f);
                 }
                 if (waveAnim++ >= radius) {
                     waveAnim = -1;
@@ -118,11 +133,12 @@ public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
         return new Vec3(0, 3, 0);
     }
 
-    protected void createTremorBlock(BlockPos blockPos) {
-        if (level.getBlockState(blockPos.below()).isFaceSturdy(level, blockPos, Direction.UP)) {
-            var fallingblockentity = new VisualFallingBlockEntity(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), level.getBlockState(blockPos));
-            fallingblockentity.setDeltaMovement(0, .1 + random.nextFloat() * .2, 0);
-            level.addFreshEntity(fallingblockentity);
+    protected void createTremorBlock(BlockPos blockPos, float impulseStrength) {
+        var fallingblockentity = new VisualFallingBlockEntity(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), level.getBlockState(blockPos));
+        fallingblockentity.setDeltaMovement(0, impulseStrength, 0);
+        level.addFreshEntity(fallingblockentity);
+        if (!level.getBlockState(blockPos.above()).isAir()) {
+            createTremorBlock(blockPos.above(), impulseStrength);
         }
     }
 
@@ -134,6 +150,14 @@ public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
                 .2f,
                 distance * Mth.sin(theta)
         );
+    }
+
+    @Override
+    public void onClientRemoval() {
+        if (level.isClientSide) {
+            clientEarthquakeOrigins.remove(this);
+        }
+        super.onClientRemoval();
     }
 
     @Override
@@ -161,5 +185,25 @@ public class EarthquakeAoe extends AoeEntity implements AntiMagicSusceptible {
     protected void readAdditionalSaveData(CompoundTag pCompound) {
         super.readAdditionalSaveData(pCompound);
         this.slownessAmplifier = pCompound.getInt("Slowness");
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void handleCameraShake(ViewportEvent.ComputeCameraAngles event) {
+        if (clientEarthquakeOrigins.isEmpty()) {
+            return;
+        }
+        var player = event.getCamera().getEntity();
+        List<EarthquakeAoe> closestPositions = clientEarthquakeOrigins.stream().sorted((o1, o2) -> (int) (o1.position().distanceToSqr(player.position()) - o2.position().distanceToSqr(player.position()))).toList();
+        var closestPos = closestPositions.get(0).position();
+        //.0039f is 1/15^2
+        float intensity = (float) Mth.lerp(closestPos.distanceToSqr(player.position()) * 0.0039f, 1, 0);
+        float f = (float) (player.tickCount + event.getPartialTick());
+        float yaw = Mth.cos(f * 1.5f) * intensity * .35f;
+        float pitch = Mth.cos(f * 2f) * intensity * .35f;
+        float roll = Mth.sin(f * 2.2f) * intensity * .35f;
+        event.setYaw(event.getYaw() + yaw);
+        event.setRoll(event.getRoll() + roll);
+        event.setPitch(event.getPitch() + pitch);
     }
 }
