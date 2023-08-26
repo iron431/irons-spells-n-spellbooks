1f1586e9edfd073531a0c4558efaa2b33c1ad6cb
iron431 <34083081+iron431@users.noreply.github.com>
2023-08-26 16:39:01 -0500

    summon ender chest spell

--- src/main/java/io/redspace/ironsspellbooks/api/registry/SpellRegistry.java
+++ src/main/java/io/redspace/ironsspellbooks/api/registry/SpellRegistry.java
@@ -95,6 +95,7 @@ public class SpellRegistry {
     public static final RegistryObject<AbstractSpell> MAGIC_MISSILE_SPELL = registerSpell(new MagicMissileSpell());
     public static final RegistryObject<AbstractSpell> STARFALL_SPELL = registerSpell(new StarfallSpell());
     public static final RegistryObject<AbstractSpell> TELEPORT_SPELL = registerSpell(new TeleportSpell());
+    public static final RegistryObject<AbstractSpell> SUMMON_ENDER_CHEST_SPELL = registerSpell(new SummonEnderChestSpell());
 
     // EVOCATION
     public static final RegistryObject<AbstractSpell> CHAIN_CREEPER_SPELL = registerSpell(new ChainCreeperSpell());
---
+++ src/main/java/io/redspace/ironsspellbooks/spells/ender/SummonEnderChestSpell.java
@@ -0,0 +1,97 @@
+package io.redspace.ironsspellbooks.spells.ender;
+
+import io.redspace.ironsspellbooks.IronsSpellbooks;
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
+import io.redspace.ironsspellbooks.entity.mobs.AntiMagicSusceptible;
+import io.redspace.ironsspellbooks.entity.mobs.MagicSummon;
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
+import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.PlayerEnderChestContainer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.util.LazyOptional;
+
+import java.util.List;
+import java.util.Optional;
+
+@AutoSpellConfig
+public class SummonEnderChestSpell extends AbstractSpell {
+    private final ResourceLocation spellId = new ResourceLocation(IronsSpellbooks.MODID, "summon_ender_chest");
+
+    public SummonEnderChestSpell() {
+        this.manaCostPerLevel = 1;
+        this.baseSpellPower = 1;
+        this.spellPowerPerLevel = 1;
+        this.castTime = 0;
+        this.baseManaCost = 25;
+    }
+
+    private final DefaultConfig defaultConfig = new DefaultConfig()
+            .setMinRarity(SpellRarity.RARE)
+            .setSchoolResource(SchoolRegistry.ENDER_RESOURCE)
+            .setMaxLevel(1)
+            .setCooldownSeconds(5)
+            .build();
+
+    @Override
+    public CastType getCastType() {
+        return CastType.INSTANT;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return defaultConfig;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastStartSound() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<SoundEvent> getCastFinishSound() {
+        return Optional.empty();
+    }
+
+    private static final Component CONTAINER_TITLE = Component.translatable("container.enderchest");
+
+    @Override
+    public void onCast(Level world, int spellLevel, LivingEntity entity, MagicData playerMagicData) {
+        if (entity instanceof Player player) {
+            PlayerEnderChestContainer playerenderchestcontainer = player.getEnderChestInventory();
+            player.openMenu(new SimpleMenuProvider((p_53124_, p_53125_, p_53126_) -> {
+                return ChestMenu.threeRows(p_53124_, p_53125_, playerenderchestcontainer);
+            }, CONTAINER_TITLE));
+        }
+        super.onCast(world, spellLevel, entity, playerMagicData);
+    }
+
+    @Override
+    public AnimationHolder getCastStartAnimation() {
+        return SpellAnimations.SELF_CAST_ANIMATION;
+    }
+}
