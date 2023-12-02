a72862a9d1ca7f8cd9aaaed9812f3909f9423a45
iron431 <34083081+iron431@users.noreply.github.com>
2023-12-02 15:50:23 -0600

    Dragonskin dropped from the dragon glows and floats

--- src/main/java/io/redspace/ironsspellbooks/mixin/ClientEntityMixin.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/ClientEntityMixin.java
@@ -1,12 +1,15 @@
 package io.redspace.ironsspellbooks.mixin;
 
+import io.redspace.ironsspellbooks.registries.ItemRegistry;
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 @Mixin(Entity.class)
 
 public class ClientEntityMixin {
@@ -18,6 +21,8 @@ public class ClientEntityMixin {
     public void changeGlowOutline(CallbackInfoReturnable<Integer> cir) {
         if (Minecraft.getInstance().player != null && Minecraft.getInstance().player.hasEffect(MobEffectRegistry.PLANAR_SIGHT.get())) {
             cir.setReturnValue(0x6c42f5);
+        } else if ((Entity) (Object) this instanceof ItemEntity item && item.getItem().is(ItemRegistry.DRAGONSKIN.get())) {
+            cir.setReturnValue(0xd21cff);
         }
     }
 }
