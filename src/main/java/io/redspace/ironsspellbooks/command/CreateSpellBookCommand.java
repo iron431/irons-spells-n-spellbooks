f3ce3073a0f34f4b1578bf2e3f042a50516eeada
iron431 <34083081+iron431@users.noreply.github.com>
2023-12-03 00:32:10 -0600

    randomized spell book command parameter

--- src/main/java/io/redspace/ironsspellbooks/command/CreateSpellBookCommand.java
+++ src/main/java/io/redspace/ironsspellbooks/command/CreateSpellBookCommand.java
@@ -1,12 +1,15 @@
 package io.redspace.ironsspellbooks.command;
 
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import io.redspace.ironsspellbooks.capabilities.spellbook.SpellBookData;
 import io.redspace.ironsspellbooks.item.SpellBook;
+import io.redspace.ironsspellbooks.loot.SpellFilter;
 import io.redspace.ironsspellbooks.registries.ItemRegistry;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import io.redspace.ironsspellbooks.spells.eldritch.AbstractEldritchSpell;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
@@ -17,11 +20,12 @@ public class CreateSpellBookCommand {
     private static final SimpleCommandExceptionType ERROR_FAILED = new SimpleCommandExceptionType(Component.translatable("commands.irons_spellbooks.create_spell_book.failed"));
 
     public static void register(CommandDispatcher<CommandSourceStack> pDispatcher) {
-        pDispatcher.register(Commands.literal("createSpellBook").requires((p_138819_) -> {
-            return p_138819_.hasPermission(2);
-        }).then(Commands.argument("slots", IntegerArgumentType.integer(1, 15)).executes((commandContext) -> {
-            return crateSpellBook(commandContext.getSource(), IntegerArgumentType.getInteger(commandContext, "slots"));
-        })));
+        pDispatcher.register(
+                Commands.literal("createSpellBook").requires((p_138819_) -> p_138819_.hasPermission(2))
+                        .then(Commands.argument("slots", IntegerArgumentType.integer(1, 20))
+                                .executes((commandContext) -> crateSpellBook(commandContext.getSource(), IntegerArgumentType.getInteger(commandContext, "slots")))
+                                .then(Commands.literal("randomize")
+                                        .executes((commandContext) -> crateRandomSpellBook(commandContext.getSource(), IntegerArgumentType.getInteger(commandContext, "slots"))))));
     }
 
     private static int crateSpellBook(CommandSourceStack source, int slots) throws CommandSyntaxException {
@@ -29,6 +33,26 @@ public class CreateSpellBookCommand {
         if (serverPlayer != null) {
             ItemStack itemstack = new ItemStack(ItemRegistry.WIMPY_SPELL_BOOK.get());
             var spellBookData = new SpellBookData(slots);
+            SpellBookData.setSpellBookData(itemstack, spellBookData);
+            if (serverPlayer.getInventory().add(itemstack)) {
+                return 1;
+            }
+        }
+
+        throw ERROR_FAILED.create();
+    }
+
+    private static int crateRandomSpellBook(CommandSourceStack source, int slots) throws CommandSyntaxException {
+        var serverPlayer = source.getPlayer();
+        if (serverPlayer != null) {
+            ItemStack itemstack = new ItemStack(ItemRegistry.WIMPY_SPELL_BOOK.get());
+            var spellBookData = new SpellBookData(slots);
+            for (int i = 0; i < slots; i++) {
+                AbstractSpell spell;
+                do {
+                    spell = new SpellFilter().getRandomSpell(source.getLevel().random);
+                } while (!spellBookData.addSpell(spell, source.getLevel().random.nextIntBetweenInclusive(1, spell.getMaxLevel()), itemstack));
+            }
             SpellBookData.setSpellBookData(itemstack, spellBookData);
             if (serverPlayer.getInventory().add(itemstack)) {
                 return 1;
