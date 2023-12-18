828452d911daa2bb9012bc4ac2790d07eb51ab2a
Christian <cmb@bitfly.org>
2023-12-18 13:28:45 -0600

    guiding bolt manager checkpoint

--- src/main/java/io/redspace/ironsspellbooks/effect/guiding_bolt/GuidingBoltManager.java
+++ src/main/java/io/redspace/ironsspellbooks/effect/guiding_bolt/GuidingBoltManager.java
@@ -1,47 +1,42 @@
 package io.redspace.ironsspellbooks.effect.guiding_bolt;
 
 import io.redspace.ironsspellbooks.IronsSpellbooks;
-import io.redspace.ironsspellbooks.api.util.CameraShakeData;
 import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.datafix.IronsSpellBooksWorldData;
+import io.redspace.ironsspellbooks.internal_event.IronsWorldDatEvent;
+import net.minecraft.nbt.*;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.checkerframework.checker.units.qual.A;
-import oshi.software.os.mac.MacOSThread;
 
 import java.util.*;
 
 @Mod.EventBusSubscriber
 public class GuidingBoltManager {
 
-    private static final HashMap<UUID, ArrayList<Projectile>> TRACKED_ENTITIES = new HashMap<>();
-
+    public static final HashMap<UUID, ArrayList<Projectile>> TRACKED_ENTITIES = new HashMap<>();
 
     @SubscribeEvent
     public static void onProjectileShot(EntityJoinLevelEvent event) {
         if (event.getLevel() instanceof ServerLevel serverLevel) {
             if (event.getEntity() instanceof Projectile projectile) {
-                IronsSpellbooks.LOGGER.debug("on projectile shoot: {}", event.getEntity());
+                Vec3 start = projectile.position();
+                int searchRange = 32;
+                Vec3 end = Utils.raycastForBlock(event.getLevel(), start, projectile.getDeltaMovement().normalize().scale(searchRange).add(start), ClipContext.Fluid.NONE).getLocation();
                 for (Map.Entry<UUID, ArrayList<Projectile>> entry : TRACKED_ENTITIES.entrySet()) {
                     var entity = serverLevel.getEntity(entry.getKey());
                     if (entity != null) {
-                        int searchRange = 32;
-                        Vec3 start = projectile.position();
                         if (Math.abs(entity.getX() - projectile.getX()) > searchRange || Math.abs(entity.getY() - projectile.getY()) > searchRange || Math.abs(entity.getZ() - projectile.getZ()) > searchRange) {
                             continue;
                         }
-                        Vec3 end = Utils.raycastForBlock(event.getLevel(), start, projectile.getDeltaMovement().normalize().scale(searchRange).add(start), ClipContext.Fluid.NONE).getLocation();
                         if (Utils.checkEntityIntersecting(entity, start, end, 3f).getType() == HitResult.Type.ENTITY) {
                             entry.getValue().add(projectile);
                         }
@@ -54,7 +49,7 @@ public class GuidingBoltManager {
     private static final int tickDelay = 3;
 
     @SubscribeEvent
-    public static void serverTick(LivingEvent.LivingTickEvent event) {
+    public static void livingTick(LivingEvent.LivingTickEvent event) {
         if (TRACKED_ENTITIES.isEmpty()) {
             return;
         }
@@ -75,13 +70,56 @@ public class GuidingBoltManager {
         }
     }
 
+//    @SubscribeEvent
+//    public static void readFromWorldDat(IronsWorldDatEvent.Load event) {
+//        CompoundTag tag = event.getTag();
+//        deserialize(tag);
+//        IronsSpellbooks.LOGGER.debug("GuidingBoltManager.readFromWorldDat map before:{}", TRACKED_ENTITIES);
+//        IronsSpellbooks.LOGGER.debug("GuidingBoltManager.readFromWorldDat tag:{}", tag);
+//
+//        IronsSpellbooks.LOGGER.debug("GuidingBoltManager.readFromWorldDat map after:{}", TRACKED_ENTITIES);
+//
+//    }
+
+//    @SubscribeEvent
+//    public static void saveToWorldDat(IronsWorldDatEvent.Save event) {
+//        if (!TRACKED_ENTITIES.isEmpty()) {
+//            serialize(event.getTag());
+//            IronsSpellbooks.LOGGER.debug("GuidingBoltManager.saveToWorldDat tag:{}", event.getTag());
+//        }
+//    }
+
+    public static void serialize(CompoundTag tag) {
+        ListTag uuids = new ListTag();
+        for (UUID key : TRACKED_ENTITIES.keySet()) {
+            uuids.add(NbtUtils.createUUID(key));
+        }
+        tag.put("GuidingBoltManager", uuids);
+    }
+
+    public static void deserialize(CompoundTag tag) {
+        if (tag.contains("GuidingBoltManager", 9)) {
+            ListTag list = tag.getList("GuidingBoltManager", 11);
+            for (Tag uuidTag : list) {
+                try {
+                    var uuid = NbtUtils.loadUUID(uuidTag);
+                    TRACKED_ENTITIES.put(uuid, new ArrayList<>());
+                } catch (Exception ignored) {
+                    continue;
+                }
+            }
+        }
+    }
+
     public static void startTracking(LivingEntity entity) {
         if (!TRACKED_ENTITIES.containsKey(entity.getUUID())) {
             TRACKED_ENTITIES.put(entity.getUUID(), new ArrayList<>());
         }
+        IronsSpellBooksWorldData.INSTANCE.guidingBoltManager = TRACKED_ENTITIES;
     }
 
     public static void stopTracking(LivingEntity entity) {
         TRACKED_ENTITIES.remove(entity.getUUID());
+        IronsSpellBooksWorldData.INSTANCE.guidingBoltManager = TRACKED_ENTITIES;
     }
 }
