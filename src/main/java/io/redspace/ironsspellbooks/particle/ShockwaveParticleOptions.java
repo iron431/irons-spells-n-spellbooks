0b6bac8ed328ba4a65203ab7248b382328d51574
Christian <cmb@bitfly.org>
2024-01-02 19:27:15 -0600

    fix shockwave particle packet

--- src/main/java/io/redspace/ironsspellbooks/particle/ShockwaveParticleOptions.java
+++ src/main/java/io/redspace/ironsspellbooks/particle/ShockwaveParticleOptions.java
@@ -75,6 +75,12 @@ public class ShockwaveParticleOptions extends DustParticleOptionsBase implements
         }
     };
 
+    @Override
+    public void writeToNetwork(FriendlyByteBuf pBuffer) {
+        super.writeToNetwork(pBuffer);
+        pBuffer.writeBoolean(fullbright);
+    }
+
     public @NotNull ParticleType<IShockwaveParticleOptions> getType() {
         return ParticleRegistry.SHOCKWAVE_PARTICLE.get();
     }
