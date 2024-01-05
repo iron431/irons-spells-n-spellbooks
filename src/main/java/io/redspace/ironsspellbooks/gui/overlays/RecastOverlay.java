aacd59be86b476b912bdd6f3927dc4c4c640b4aa
Christian <cmb@bitfly.org>
2024-01-05 00:38:15 -0600

    recast overlay overhaul

--- src/main/java/io/redspace/ironsspellbooks/gui/overlays/RecastOverlay.java
+++ src/main/java/io/redspace/ironsspellbooks/gui/overlays/RecastOverlay.java
@@ -2,6 +2,7 @@ package io.redspace.ironsspellbooks.gui.overlays;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
 import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.player.ClientMagicData;
@@ -27,20 +28,31 @@ public class RecastOverlay extends GuiComponent {
     public static void render(ForgeGui gui, PoseStack poseStack, float partialTick, int screenWidth, int screenHeight) {
         if (!ClientMagicData.getRecasts().hasRecastsActive())
             return;
-        int totalHeightPerBar = 12;
-        int screenTopBuffer = 4;
+        int totalHeightPerBar = 18;
+        int screenTopBuffer = 6;
         var activeRecasts = ClientMagicData.getRecasts().getActiveRecasts();
 
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-        RenderSystem.setShaderTexture(0, TEXTURE);
+
         for (int castIndex = 0; castIndex < activeRecasts.size(); castIndex++) {
             var recastInstance = activeRecasts.get(castIndex);
+            var spell = SpellRegistry.getSpell(recastInstance.getSpellId());
             int total = recastInstance.getTotalRecasts();
             int remaining = recastInstance.getRemainingRecasts();
             int totalWidth = total * ORB_WIDTH + (total - 1) * CONNECTOR_WIDTH;
             int barX = (screenWidth - totalWidth) / 2;
             int barY = screenTopBuffer + totalHeightPerBar * castIndex;
+
+            poseStack.pushPose();
+            poseStack.translate(barX - 18, barY - 2, 0);
+            poseStack.scale(0.85f, 0.85f, 0.85f);
+            RenderSystem.setShaderTexture(0, spell.getSpellIconResource());
+            blit(poseStack, 0, 0, 0, 0, 16, 16, 16, 16);
+            RenderSystem.setShaderTexture(0, TEXTURE);
+            blit(poseStack, -2, -2, 116, 0, 20, 20, 256, 256);
+            poseStack.popPose();
+
             for (int i = 0; i < total; i++) {
                 int orbX = barX + (ORB_WIDTH + CONNECTOR_WIDTH) * i;
                 int connectorX = orbX + ORB_WIDTH;
@@ -58,15 +70,34 @@ public class RecastOverlay extends GuiComponent {
 //                    blit(poseStack, orbX, barY, ORB_TEXTURE_OFFSET_X + 10, ORB_TEXTURE_OFFSET_Y + 21, ORB_WIDTH, ORB_WIDTH, 256, 256);
 //                    RenderSystem.setShader(GameRenderer::getPositionTexShader);
 //                    RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-                    int color = SpellRegistry.getSpell(recastInstance.getSpellId()).getSchoolType().getDisplayName().getStyle().getColor().getValue();
-                    fillGradient(poseStack, orbX + 3, barY + 3, orbX + 7, barY + 7, color, color, 0);
+                    Vector3f color = spell.getSchoolType().getTargetingColor();
+                    RenderSystem.setShaderColor(color.x(), color.y(), color.z(), 1f);
+                    blit(poseStack, orbX, barY, ORB_TEXTURE_OFFSET_X + (charged ? 0 : 10), ORB_TEXTURE_OFFSET_Y + 21, ORB_WIDTH, ORB_WIDTH, 256, 256);
+                    RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+                    //int color = SpellRegistry.getSpell(recastInstance.getSpellId()).getSchoolType().getDisplayName().getStyle().getColor().getValue();
+                    //fillGradient(poseStack, orbX + 3, barY + 3, orbX + 7, barY + 7, color, color, 0);
                 }
                 //orb
                 blit(poseStack, orbX, barY, ORB_TEXTURE_OFFSET_X, ORB_TEXTURE_OFFSET_Y, ORB_WIDTH, ORB_WIDTH, 256, 256);
             }
 
             int textX = (barX + (ORB_WIDTH + CONNECTOR_WIDTH) * total);
-            gui.getFont().draw(poseStack, String.format("%ds",recastInstance.getTicksRemaining()/20), textX, barY, ChatFormatting.WHITE.getColor());
+            gui.getFont().draw(poseStack,formatTime(recastInstance.getTicksRemaining(), recastInstance.getTicksToLive()), textX, barY + (ORB_WIDTH - gui.getFont().lineHeight) / 2, ChatFormatting.WHITE.getColor());
         }
+    }
+
+    private static String formatTime(int ticksRemaining, int totalTicks) {
+        var totalSeconds = totalTicks / 20;
+        var remainingSeconds = ticksRemaining / 20;
+        String time = "";
+        if (totalSeconds > 60) {
+            time += String.format("%s:", remainingSeconds / 60);
+            remainingSeconds %= 60;
+        }
+        if (totalSeconds >= 10) {
+            time += remainingSeconds / 10;
+        }
+        time += remainingSeconds % 10;
+        return time +"s";
     }
 }
