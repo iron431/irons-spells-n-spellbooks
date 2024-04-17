7f3ead705b863d3bf04f8befa78e5ad9bb62802f
IGN-Styly <claudioresendeptbr@gmail.com>
2024-04-17 20:38:57 +0100

    comply with review

--- src/main/java/io/redspace/ironsspellbooks/api/util/UpdateClient.java
+++ src/main/java/io/redspace/ironsspellbooks/api/util/UpdateClient.java
@@ -1,12 +1,15 @@
-package io.redspace.ironsspellbooks.api.network.spells;
+package io.redspace.ironsspellbooks.api.util;
 
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.network.ClientboundSyncMana;
 import io.redspace.ironsspellbooks.setup.Messages;
 import net.minecraft.server.level.ServerPlayer;
 
-public class ServerBoundManaUpdate {
-    public ServerBoundManaUpdate(ServerPlayer serverPlayer, MagicData magicData){
+public class UpdateClient {
+    // More Util updates to be added
+    public static void SendManaUpdate(ServerPlayer serverPlayer, MagicData magicData){
         Messages.sendToPlayer(new ClientboundSyncMana(magicData),serverPlayer);
     }
+
+
 }
