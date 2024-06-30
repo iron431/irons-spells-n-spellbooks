7b4dbe47f721572c126044b4ebd33d4c7b36736c
iron431 <34083081+iron431@users.noreply.github.com>
2024-06-30 09:46:14 -0500

    fix cast command client reset bug

--- src/main/java/io/redspace/ironsspellbooks/api/magic/MagicData.java
+++ src/main/java/io/redspace/ironsspellbooks/api/magic/MagicData.java
@@ -267,7 +267,7 @@ public class MagicData {
             }
             return new MagicData(serverPlayer);
         } else {
-            return new MagicData(null);
+            return new MagicData(true);
         }
     }
 
