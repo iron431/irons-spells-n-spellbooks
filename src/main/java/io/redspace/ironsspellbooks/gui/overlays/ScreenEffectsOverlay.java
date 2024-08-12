247e12078dafbb1883f62e445d4e2a54d688b190
iron431 <34083081+iron431@users.noreply.github.com>
2024-08-11 19:39:29 -0500

    fix necromancer spawns

--- src/main/java/io/redspace/ironsspellbooks/gui/overlays/ScreenEffectsOverlay.java
+++ src/main/java/io/redspace/ironsspellbooks/gui/overlays/ScreenEffectsOverlay.java
@@ -3,6 +3,7 @@ package io.redspace.ironsspellbooks.gui.overlays;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import io.redspace.ironsspellbooks.IronsSpellbooks;
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
@@ -26,6 +27,9 @@ public class ScreenEffectsOverlay implements LayeredDraw.Layer {
         }
         var screenWidth = guiHelper.guiWidth();
         var screenHeight = guiHelper.guiHeight();
+
+        //guiHelper.drawString(Minecraft.getInstance().font, String.format("ice:   %s", Minecraft.getInstance().player.getAttributeValue(AttributeRegistry.ICE_SPELL_POWER)), 10, 10, 0xFFFFFF);
+        //guiHelper.drawString(Minecraft.getInstance().font, String.format("blood: %s", Minecraft.getInstance().player.getAttributeValue(AttributeRegistry.BLOOD_SPELL_POWER)), 10, 20, 0xFFFFFF);
 
         Player player = Minecraft.getInstance().player;
         if (player == null) {
