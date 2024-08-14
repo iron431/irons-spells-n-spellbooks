fa36572d447bb4a7dbddbd506aa85a3b98b9decf
iron431 <34083081+iron431@users.noreply.github.com>
2024-08-14 16:57:39 -0500

    restructure bones

--- src/main/java/io/redspace/ironsspellbooks/entity/dragon/TestDragonAnimation.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/dragon/TestDragonAnimation.java
@@ -6,32 +6,31 @@ import net.minecraft.client.animation.Keyframe;
 import net.minecraft.client.animation.KeyframeAnimations;
 
 /**
- * Made with Blockbench 4.10.3
+ * Made with Blockbench 4.10.4
  * Exported for Minecraft version 1.19 or later with Mojang mappings
  * @author Author
  */
 public class TestDragonAnimation {
 	public static final AnimationDefinition test_animation = AnimationDefinition.Builder.withLength(3.0F)
-		.addAnimation("neck_base", new AnimationChannel(AnimationChannel.Targets.ROTATION,
-			new Keyframe(0.4583F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.4583F, KeyframeAnimations.degreeVec(0.0F, 35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
-			new Keyframe(3.4583F, KeyframeAnimations.degreeVec(0.0F, -35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
-		))
-		.addAnimation("neck", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
-			new Keyframe(0.25F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.25F, KeyframeAnimations.degreeVec(0.0F, 35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
-			new Keyframe(3.25F, KeyframeAnimations.degreeVec(0.0F, -35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
-		))
-		.addAnimation("head", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
-			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(0.7917F, KeyframeAnimations.degreeVec(0.0F, 35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
-			new Keyframe(1.4583F, KeyframeAnimations.degreeVec(0.0F, -87.5F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
-			new Keyframe(2.9583F, KeyframeAnimations.degreeVec(0.0F, -37.5F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
-		))
-		.build();
+			.addAnimation("neck_base", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.4583F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(1.4583F, KeyframeAnimations.degreeVec(0.0F, 35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(3.4583F, KeyframeAnimations.degreeVec(0.0F, -35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+			))
+			.addAnimation("neck", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.25F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(1.25F, KeyframeAnimations.degreeVec(0.0F, 35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(3.25F, KeyframeAnimations.degreeVec(0.0F, -35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+			))
+			.addAnimation("head", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(0.7917F, KeyframeAnimations.degreeVec(0.0F, 35.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(1.4583F, KeyframeAnimations.degreeVec(0.0F, -87.5F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(2.9583F, KeyframeAnimations.degreeVec(0.0F, -37.5F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+			))
+			.build();
 
-	public static final AnimationDefinition walk = AnimationDefinition.Builder.withLength(2.0F)
-			.looping()
+	public static final AnimationDefinition walk = AnimationDefinition.Builder.withLength(2.0F).looping()
 			.addAnimation("right_leg", new AnimationChannel(AnimationChannel.Targets.ROTATION,
 					new Keyframe(0.0F, KeyframeAnimations.degreeVec(22.5F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
 					new Keyframe(0.75F, KeyframeAnimations.degreeVec(8.86F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
@@ -65,6 +64,18 @@ public class TestDragonAnimation {
 					new Keyframe(0.0F, KeyframeAnimations.degreeVec(50.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
 					new Keyframe(1.0F, KeyframeAnimations.degreeVec(-12.5F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
 					new Keyframe(2.0F, KeyframeAnimations.degreeVec(50.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+			))
+			.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -7.5F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(1.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 7.5F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(2.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -7.5F), AnimationChannel.Interpolations.CATMULLROM)
+			))
+			.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.POSITION,
+					new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 2.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(0.5417F, KeyframeAnimations.posVec(0.0F, -1.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(1.0F, KeyframeAnimations.posVec(0.0F, 2.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(1.5417F, KeyframeAnimations.posVec(0.0F, -1.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM),
+					new Keyframe(2.0F, KeyframeAnimations.posVec(0.0F, 2.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
 			))
 			.build();
 }
--- src/main/java/io/redspace/ironsspellbooks/entity/dragon/TestDragonModel.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/dragon/TestDragonModel.java
@@ -10,6 +10,8 @@ import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.List;
+
 public class TestDragonModel extends HierarchicalModel<DragonEntity> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "dragon_final"), "main");
@@ -21,7 +23,8 @@ public class TestDragonModel extends HierarchicalModel<DragonEntity> {
     private final ModelPart jaw;
     private final ModelPart left_wing;
     private final ModelPart left_wing_tip;
-    private final ModelPart torso;
+    private final ModelPart root;
+    private final ModelPart body;
     private final ModelPart tail;
     private final ModelPart tail_tip;
     private final ModelPart right_leg;
@@ -32,42 +35,51 @@ public class TestDragonModel extends HierarchicalModel<DragonEntity> {
     private final ModelPart left_foot;
 
     public TestDragonModel(ModelPart root) {
-        this.torso = root.getChild("torso");
-        this.right_wing = torso.getChild("right_wing");
-        this.right_wing_tip = right_wing.getChild("right_wing_tip");
-        this.neck_base = torso.getChild("neck_base");
-        this.neck = neck_base.getChild("neck");
-        this.head = neck.getChild("head");
-        this.jaw = head.getChild("jaw");
-        this.left_wing = torso.getChild("left_wing");
-        this.left_wing_tip = left_wing.getChild("left_wing_tip");
-        this.tail = torso.getChild("tail");
-        this.tail_tip = tail.getChild("tail_tip");
-        this.right_leg = torso.getChild("right_leg");
-        this.right_leg_lower = right_leg.getChild("right_leg_lower");
-        this.right_foot = right_leg_lower.getChild("right_foot");
-        this.left_leg = torso.getChild("left_leg");
-        this.left_leg_lower = left_leg.getChild("left_leg_lower");
-        this.left_foot = left_leg_lower.getChild("left_foot");
+        var parts = root.getAllParts().toList();
+
+        this.root = root;
+        this.body = getOrThrow(parts, "body");
+        this.left_wing = getOrThrow(parts, "left_wing");
+        this.left_wing_tip = getOrThrow(parts, "left_wing_tip");
+        this.neck_base = getOrThrow(parts, "neck_base");
+        this.neck = getOrThrow(parts, "neck");
+        this.head = getOrThrow(parts, "head");
+        this.jaw = getOrThrow(parts, "jaw");
+        this.right_wing = getOrThrow(parts, "right_wing");
+        this.right_wing_tip = getOrThrow(parts, "right_wing_tip");
+        this.tail = getOrThrow(parts, "tail");
+        this.tail_tip = getOrThrow(parts, "tail_tip");
+        this.right_leg = getOrThrow(parts, "right_leg");
+        this.right_leg_lower = getOrThrow(parts, "right_leg_lower");
+        this.right_foot = getOrThrow(parts, "right_foot");
+        this.left_leg = getOrThrow(parts, "left_leg");
+        this.left_leg_lower = getOrThrow(parts, "left_leg_lower");
+        this.left_foot = getOrThrow(parts, "left_foot");
+    }
+
+    public static ModelPart getOrThrow(List<ModelPart> parts, String name) {
+        return parts.stream().filter(part -> part.hasChild(name)).findFirst().get().getChild(name);
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition torso = partdefinition.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 34).addBox(-10.0F, -40.0F, -21.0F, 20.0F, 20.0F, 44.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 60).mirror().addBox(5.0F, -42.0F, -20.0F, 0.0F, 2.0F, 38.0F, new CubeDeformation(0.0F)).mirror(false)
-                .texOffs(0, 60).addBox(-5.0F, -42.0F, -20.0F, 0.0F, 2.0F, 38.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition left_wing = torso.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(0, 111).addBox(0.0F, -3.0F, -3.0F, 35.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
+        PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 34).addBox(-10.0F, -10.0F, -21.0F, 20.0F, 20.0F, 44.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 60).mirror().addBox(5.0F, -12.0F, -20.0F, 0.0F, 2.0F, 38.0F, new CubeDeformation(0.0F)).mirror(false)
+                .texOffs(0, 60).addBox(-5.0F, -12.0F, -20.0F, 0.0F, 2.0F, 38.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -30.0F, 0.0F));
+
+        PartDefinition left_wing = body.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(0, 111).addBox(0.0F, -3.0F, -3.0F, 35.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
                 .texOffs(63, 34).addBox(-1.0F, -1.0F, 3.0F, 36.0F, 0.0F, 21.0F, new CubeDeformation(0.0F))
                 .texOffs(48, 144).addBox(30.0F, 3.0F, -3.0F, 5.0F, 6.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(61, 123).addBox(30.0F, 7.0F, -6.0F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(10.0F, -37.0F, -13.0F));
+                .texOffs(61, 123).addBox(30.0F, 7.0F, -6.0F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(10.0F, -7.0F, -13.0F));
 
         PartDefinition left_wing_tip = left_wing.addOrReplaceChild("left_wing_tip", CubeListBuilder.create().texOffs(0, 105).addBox(0.0F, -1.0F, -1.0F, 59.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
                 .texOffs(0, 0).addBox(0.0F, 0.0F, 2.0F, 56.0F, 0.0F, 34.0F, new CubeDeformation(0.0F)), PartPose.offset(35.0F, -1.0F, -1.0F));
 
-        PartDefinition neck_base = torso.addOrReplaceChild("neck_base", CubeListBuilder.create().texOffs(110, 137).addBox(-6.0F, -6.0F, -11.0F, 12.0F, 14.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -32.0F, -21.0F));
+        PartDefinition neck_base = body.addOrReplaceChild("neck_base", CubeListBuilder.create().texOffs(110, 137).addBox(-6.0F, -6.0F, -11.0F, 12.0F, 14.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.0F, -21.0F));
 
         PartDefinition neck = neck_base.addOrReplaceChild("neck", CubeListBuilder.create().texOffs(102, 105).addBox(-4.0F, -3.0F, -22.0F, 8.0F, 10.0F, 22.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -3.0F, -11.0F));
 
@@ -92,19 +104,19 @@ public class TestDragonModel extends HierarchicalModel<DragonEntity> {
                 .texOffs(0, 158).addBox(-4.0F, -2.0F, -21.0F, 8.0F, 3.0F, 11.0F, new CubeDeformation(-0.05F))
                 .texOffs(0, 172).addBox(-6.0F, -4.0F, -10.0F, 12.0F, 4.0F, 12.0F, new CubeDeformation(-0.05F)), PartPose.offset(0.0F, 3.0F, 2.0F));
 
-        PartDefinition right_wing = torso.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(63, 34).mirror().addBox(-35.0F, -1.0F, 3.0F, 36.0F, 0.0F, 21.0F, new CubeDeformation(0.0F)).mirror(false)
+        PartDefinition right_wing = body.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(63, 34).mirror().addBox(-35.0F, -1.0F, 3.0F, 36.0F, 0.0F, 21.0F, new CubeDeformation(0.0F)).mirror(false)
                 .texOffs(0, 111).mirror().addBox(-35.0F, -3.0F, -3.0F, 35.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false)
                 .texOffs(48, 144).mirror().addBox(-35.0F, 3.0F, -3.0F, 5.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
-                .texOffs(61, 123).addBox(-35.0F, 7.0F, -6.0F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-10.0F, -37.0F, -13.0F));
+                .texOffs(61, 123).addBox(-35.0F, 7.0F, -6.0F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-10.0F, -7.0F, -13.0F));
 
         PartDefinition right_wing_tip = right_wing.addOrReplaceChild("right_wing_tip", CubeListBuilder.create().texOffs(0, 105).mirror().addBox(-59.0F, -1.0F, -1.0F, 59.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false)
                 .texOffs(0, 0).mirror().addBox(-56.0F, 0.0F, 2.0F, 56.0F, 0.0F, 34.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-35.0F, -1.0F, -1.0F));
 
-        PartDefinition tail = torso.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(63, 118).addBox(-5.0F, -5.0F, -2.0F, 10.0F, 11.0F, 19.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -32.0F, 23.0F));
+        PartDefinition tail = body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(63, 118).addBox(-5.0F, -5.0F, -2.0F, 10.0F, 11.0F, 19.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.0F, 23.0F));
 
         PartDefinition tail_tip = tail.addOrReplaceChild("tail_tip", CubeListBuilder.create().texOffs(87, 57).addBox(-3.0F, -1.85F, -2.0F, 6.0F, 7.0F, 41.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -3.0F, 18.0F));
 
-        PartDefinition right_leg = torso.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 123).addBox(-5.0F, -7.0F, -7.0F, 10.0F, 21.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(-12.0F, -28.0F, 14.0F));
+        PartDefinition right_leg = root.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 123).addBox(-5.0F, -7.0F, -7.0F, 10.0F, 21.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(-12.0F, -28.0F, 14.0F));
 
         PartDefinition right_leg_lower = right_leg.addOrReplaceChild("right_leg_lower", CubeListBuilder.create().texOffs(0, 38).addBox(-3.0F, -3.0F, -3.0F, 7.0F, 17.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 10.0F, 5.0F));
 
@@ -113,7 +125,7 @@ public class TestDragonModel extends HierarchicalModel<DragonEntity> {
                 .texOffs(18, 0).addBox(-1.0F, 0.0F, -13.0F, 3.0F, 4.0F, 5.0F, new CubeDeformation(0.0F))
                 .texOffs(0, 22).addBox(-4.0F, 0.0F, -8.0F, 9.0F, 4.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 14.0F, 1.0F));
 
-        PartDefinition left_leg = torso.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 123).mirror().addBox(-5.0F, -7.0F, -7.0F, 10.0F, 21.0F, 14.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(12.0F, -28.0F, 14.0F));
+        PartDefinition left_leg = root.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 123).mirror().addBox(-5.0F, -7.0F, -7.0F, 10.0F, 21.0F, 14.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(12.0F, -28.0F, 14.0F));
 
         PartDefinition left_leg_lower = left_leg.addOrReplaceChild("left_leg_lower", CubeListBuilder.create().texOffs(0, 38).mirror().addBox(-4.0F, -3.0F, -3.0F, 7.0F, 17.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 10.0F, 5.0F));
 
@@ -125,11 +137,12 @@ public class TestDragonModel extends HierarchicalModel<DragonEntity> {
         return LayerDefinition.create(meshdefinition, 256, 256);
     }
 
+
     @Override
     public void setupAnim(DragonEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.root().getAllParts().forEach(ModelPart::resetPose);
         this.animate(entity.testAnimationState, TestDragonAnimation.test_animation, ageInTicks);
-        this.animateWalk(TestDragonAnimation.walk, limbSwing, limbSwingAmount, 1.0F, 2.5F);
+        this.animateWalk(TestDragonAnimation.walk, limbSwing, limbSwingAmount, 1.5F, 2.5F);
 
 //        float swingStrength = .5f;
 //        float offsetStrength = 2f;
@@ -165,6 +178,6 @@ public class TestDragonModel extends HierarchicalModel<DragonEntity> {
 
     @Override
     public ModelPart root() {
-        return this.torso;
+        return this.root;
     }
 }
