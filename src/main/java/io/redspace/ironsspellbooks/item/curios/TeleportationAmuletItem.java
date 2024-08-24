1dc6d9ae8001f954cffe24adb653d492c0ad0da2
iron431 <34083081+iron431@users.noreply.github.com>
2024-08-24 13:03:30 -0500

    reformat teleportation amulet tooltip

--- src/main/java/io/redspace/ironsspellbooks/item/curios/TeleportationAmuletItem.java
+++ src/main/java/io/redspace/ironsspellbooks/item/curios/TeleportationAmuletItem.java
@@ -7,7 +7,6 @@ import io.redspace.ironsspellbooks.compat.Curios;
 import io.redspace.ironsspellbooks.util.MinecraftInstanceHelper;
 import io.redspace.ironsspellbooks.util.ParticleHelper;
 import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
@@ -16,7 +15,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import top.theillusivec4.curios.api.CuriosApi;
@@ -48,27 +46,21 @@ public class TeleportationAmuletItem extends SimpleDescriptiveCurio {
     }
 
     @Override
-    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
-        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
-        pTooltipComponents.add(1, VANITY_DESCRIPTION);
-    }
-
-    @Override
-    public Component getDescription(ItemStack stack) {
+    public List<Component> getSlotsTooltip(List<Component> tooltips, ItemStack stack) {
+        tooltips.add(VANITY_DESCRIPTION);
         var player = MinecraftInstanceHelper.getPlayer();
         if (player != null) {
-            if (!canUse(player)) {
-                var desc = super.getDescription(stack);
-                var font = desc.getStyle().withFont(Minecraft.ALT_FONT);
-                return desc.copy().withStyle(font);
+            if (canUse(player)) {
+                return super.getSlotsTooltip(tooltips, stack);
             }
         }
-        return super.getDescription(stack);
+        return tooltips;
     }
 
     @Override
     public void curioTick(SlotContext slotContext, ItemStack stack) {
         super.curioTick(slotContext, stack);
+        //Using short modulo on tick because #onEquip fires before the living entity's attributes are calculated on relog/entity creation, meaning the curse will erroneously fire
         if (slotContext.entity().tickCount % 5 == 0) {
             handleCurse(slotContext, stack);
         }
@@ -77,13 +69,6 @@ public class TeleportationAmuletItem extends SimpleDescriptiveCurio {
     private boolean canUse(LivingEntity livingEntity) {
         return livingEntity.getAttributeValue(AttributeRegistry.ENDER_SPELL_POWER) > 1.25;
     }
-
-//    @Override
-//    public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack) {
-//        super.onEquip(slotContext, prevStack, stack);
-//        handleCurse(slotContext, stack);
-//    }
-
 
     private void createItemEntity(Level level, ItemStack stack, Vec3 center) {
         Vec3 placement = Utils.moveToRelativeGroundLevel(level, center.add(new Vec3(Utils.random.nextIntBetweenInclusive(4, 8) + Utils.random.nextFloat(), 0, 0).yRot(Utils.random.nextFloat() * Mth.TWO_PI)), 5).add(0, 0.75, 0);
