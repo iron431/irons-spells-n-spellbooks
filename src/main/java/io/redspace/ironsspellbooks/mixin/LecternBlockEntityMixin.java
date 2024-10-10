6eb60fc54b344d57252904ec4fc94005fc9c419f
iron431 <34083081+iron431@users.noreply.github.com>
2024-10-10 16:42:33 -0500

    make lectern mixin more compatible with other mods

--- src/main/java/io/redspace/ironsspellbooks/mixin/LecternBlockEntityMixin.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/LecternBlockEntityMixin.java
@@ -4,7 +4,6 @@ import io.redspace.ironsspellbooks.item.ILecternPlaceable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
@@ -63,29 +62,11 @@ public abstract class LecternBlockEntityMixin extends BlockEntity {
 
     @Override
     public ClientboundBlockEntityDataPacket getUpdatePacket() {
-        var packet = ClientboundBlockEntityDataPacket.create(this);
-        return packet;
-    }
-
-    @Override
-    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt, HolderLookup.Provider lookupProvider) {
-        handleUpdateTag(pkt.getTag(), lookupProvider);
-        if (level != null) {
-            level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), Block.UPDATE_ALL);
-        }
+        return ClientboundBlockEntityDataPacket.create(this);
     }
 
     @Override
     public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
-        CompoundTag tag = new CompoundTag();
-        saveAdditional(tag, pRegistries);
-        return tag;
-    }
-
-    @Override
-    public void handleUpdateTag(CompoundTag tag, HolderLookup.Provider lookupProvider) {
-        if (tag != null) {
-            loadAdditional(tag, lookupProvider);
-        }
+        return saveWithoutMetadata(pRegistries);
     }
 }
