829292f23e9f69761c1f706919479c4131a01fb4
iron431 <34083081+iron431@users.noreply.github.com>
2025-02-22 14:13:12 -0600

    rework armor piles

--- src/main/java/io/redspace/ironsspellbooks/block/ArmorPileBlock.java
+++ src/main/java/io/redspace/ironsspellbooks/block/ArmorPileBlock.java
@@ -34,7 +34,7 @@ public class ArmorPileBlock extends Block implements SimpleWaterloggedBlock {
     private static final VoxelShape BASE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 4.0D, 14.0D);
 
     public ArmorPileBlock() {
-        super(BlockBehaviour.Properties.of().strength(5f, 8f).requiresCorrectToolForDrops().noOcclusion().sound(SoundType.CHAIN).mapColor(MapColor.COLOR_BLACK));
+        super(BlockBehaviour.Properties.of().strength(5f, 8f).noOcclusion().sound(SoundType.CHAIN).mapColor(MapColor.COLOR_BLACK));
         this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false));
     }
 
