d5f0c13f7760e081d48bb15aeaec0157071ba172
iron431 <34083081+iron431@users.noreply.github.com>
2025-03-08 15:28:55 -0600

    3.9.0: percentage based attributes, bump version

--- src/main/java/io/redspace/ironsspellbooks/api/attribute/MagicPercentAttribute.java
+++ src/main/java/io/redspace/ironsspellbooks/api/attribute/MagicPercentAttribute.java
@@ -1,9 +1,8 @@
 package io.redspace.ironsspellbooks.api.attribute;
 
-/**
- * Serves as marker class for Apothic Attributes mixin injection (on top of MagicRangedAttribute)
- */
-public class MagicPercentAttribute extends MagicRangedAttribute {
+import net.neoforged.neoforge.common.PercentageAttribute;
+
+public class MagicPercentAttribute extends PercentageAttribute implements IMagicAttribute {
     public MagicPercentAttribute(String pDescriptionId, double pDefaultValue, double pMin, double pMax) {
         super(pDescriptionId, pDefaultValue, pMin, pMax);
     }
