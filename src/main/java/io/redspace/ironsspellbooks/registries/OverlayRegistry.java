4e041109873f9a791aab4025208682b0a60036fc
iron431 <34083081+iron431@users.noreply.github.com>
2025-04-26 18:07:13 -0500

    remove active spell overlay

--- src/main/java/io/redspace/ironsspellbooks/registries/OverlayRegistry.java
+++ src/main/java/io/redspace/ironsspellbooks/registries/OverlayRegistry.java
@@ -12,19 +12,16 @@ import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 @EventBusSubscriber(modid = IronsSpellbooks.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 
 public class OverlayRegistry {
-@SubscribeEvent
+    @SubscribeEvent
     public static void onRegisterOverlays(RegisterGuiLayersEvent event) {
-        //Ironsspellbooks.logger.debug("Registering Overlays");
 
         event.registerBelow(VanillaGuiLayers.CROSSHAIR, IronsSpellbooks.id("cast_bar"), CastBarOverlay.instance);
-//
         event.registerAbove(VanillaGuiLayers.AIR_LEVEL, IronsSpellbooks.id("mana_overlay"), ManaBarOverlay.instance);
         event.registerAbove(VanillaGuiLayers.EXPERIENCE_BAR, IronsSpellbooks.id("spell_bar"), SpellBarOverlay.instance);
-        event.registerAbove(VanillaGuiLayers.EXPERIENCE_BAR, IronsSpellbooks.id("scroll_overlay"), ActiveSpellOverlay.instance);
         event.registerAbove(VanillaGuiLayers.EXPERIENCE_BAR, IronsSpellbooks.id("recast_bar"), RecastOverlay.instance);
 
         event.registerAboveAll(IronsSpellbooks.id("spell_wheel"), SpellWheelOverlay.instance);
-    event.registerAboveAll(IronsSpellbooks.id("screen_effects"), ScreenEffectsOverlay.instance);
-    event.registerAboveAll(IronsSpellbooks.id("screen_tooltip"), ScreenTooltipOverlay.instance);
+        event.registerAboveAll(IronsSpellbooks.id("screen_effects"), ScreenEffectsOverlay.instance);
+        event.registerAboveAll(IronsSpellbooks.id("screen_tooltip"), ScreenTooltipOverlay.instance);
     }
 }
