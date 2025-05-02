79a49f9fac3ef367cbdcc14a17fc2c36198733a6
iron431 <34083081+iron431@users.noreply.github.com>
2025-05-02 13:51:16 -0500

    balance items, spellbreaker texture, changelog

--- src/main/java/io/redspace/ironsspellbooks/item/armor/PaladinArmorItem.java
+++ src/main/java/io/redspace/ironsspellbooks/item/armor/PaladinArmorItem.java
@@ -14,7 +14,6 @@ public class PaladinArmorItem extends ImbuableChestplateArmorItem {
     public PaladinArmorItem(Type type, Properties settings) {
         super(ArmorMaterialRegistry.PALADIN, type, settings,
                 new AttributeContainer(AttributeRegistry.MAX_MANA, 150, AttributeModifier.Operation.ADD_VALUE),
-                new AttributeContainer(AttributeRegistry.SPELL_RESIST, 0.25, AttributeModifier.Operation.ADD_MULTIPLIED_BASE),
                 new AttributeContainer(AttributeRegistry.SPELL_POWER, 0.10, AttributeModifier.Operation.ADD_MULTIPLIED_BASE)
         );
     }
