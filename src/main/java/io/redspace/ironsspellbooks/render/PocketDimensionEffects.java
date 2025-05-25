7d7e79a9673a15a0970ce55eb5eaed069f78d168
iron431 <34083081+iron431@users.noreply.github.com>
2025-05-25 13:56:03 -0500

    cloud nebula effect checkpoint

--- src/main/java/io/redspace/ironsspellbooks/render/PocketDimensionEffects.java
+++ src/main/java/io/redspace/ironsspellbooks/render/PocketDimensionEffects.java
@@ -1,5 +1,6 @@
 package io.redspace.ironsspellbooks.render;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Axis;
@@ -20,7 +21,7 @@ import java.util.function.Supplier;
 
 public class PocketDimensionEffects extends DimensionSpecialEffects {
     public PocketDimensionEffects() {
-        super(Float.NaN, false, DimensionSpecialEffects.SkyType.NONE, false, true);
+        super(Float.NaN, false, DimensionSpecialEffects.SkyType.NONE, false, false);
     }
 
     @Override
@@ -35,6 +36,7 @@ public class PocketDimensionEffects extends DimensionSpecialEffects {
 
     public static final ResourceLocation SKY_LOCATION = IronsSpellbooks.id("textures/environment/pocket_dimension_sky.png");
     public static final ResourceLocation CLOUDS_LOCATION = IronsSpellbooks.id("textures/environment/pocket_clouds.png");
+    public static final ResourceLocation WISP_LOCATION = IronsSpellbooks.id("textures/environment/single_cloud.png");
 
     @Override
     public boolean renderSky(ClientLevel level, int ticks, float partialTick, Matrix4f modelViewMatrix, Camera camera, Matrix4f projectionMatrix, boolean isFoggy, Runnable setupFog) {
@@ -45,10 +47,10 @@ public class PocketDimensionEffects extends DimensionSpecialEffects {
         RenderSystem.depthMask(true);
 
         Tesselator tesselator = Tesselator.getInstance();
-        float skyDistance = 80;
+        float skyDistance = 100;
         renderBox(poseStack, tesselator, skyDistance, 0, 16, GameRenderer::getPositionTexColorShader, SKY_LOCATION);
         float f = ticks + partialTick;
-        float scale = .70f; // give buffer so rotated cubes don't clip through main skybox
+        float scale = .80f; // give buffer so rotated cubes don't clip through main skybox
         int layers = 8;
         Random random = new Random(431);
         for (int i = 0; i < layers; i++) {
@@ -63,9 +65,42 @@ public class PocketDimensionEffects extends DimensionSpecialEffects {
             poseStack.mulPose(Axis.ZP.rotationDegrees(z));
             Vector3f rgb = new Vector3f(1, 1, 1);//new Vector3f(random.nextFloat() * 0.5f + 0.5f, random.nextFloat() * 0.5f + 0.5f, random.nextFloat() * 0.5f + 0.5f);
             RenderSystem.setShaderColor(rgb.x, rgb.y, rgb.z, Mth.lerp(j / (float) layers, 0.05f, 1f));
-            renderBox(poseStack, tesselator, skyDistance * scale, 0, 1 / (scale * scale), GameRenderer::getPositionTexColorShader, CLOUDS_LOCATION);
+            renderBox(poseStack, tesselator, skyDistance * scale, 0, 1f / (scale * scale), GameRenderer::getPositionTexColorShader, CLOUDS_LOCATION);
             poseStack.popPose();
             scale -= 0.04f; // give slight separation between layers to prevent too much zfighting/artifacting
+        }
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+        var color = new Vector3f(.1f, .4f, .6f);
+        color.mul(0.5f);
+        RenderSystem.setShaderColor(color.x, color.y, color.z, 1f);
+        int clouds = 20;
+        float zoff = 0; // use ever-enclosing z offset to ensure new planes are always in front of old planes, preventing alpha clipping
+        for (int i = 0; i < clouds; i++) {
+            float clusterScale = 0.05f + i * 0.002f;
+            poseStack.pushPose();
+            int count = (i + 1);
+            float speed = 0.005f;
+            float x = (random.nextInt(360) + f * speed) % 360;
+            float y = (random.nextInt(360) + f * speed) % 360;
+            float z = (random.nextInt(360) + f * speed) % 360;
+            poseStack.mulPose(Axis.XP.rotationDegrees(x));
+            poseStack.mulPose(Axis.YP.rotationDegrees(y));
+            poseStack.mulPose(Axis.ZP.rotationDegrees(z));
+            for (int j = 0; j < count; j++) {
+                Vector3f offset = new Vector3f(
+                        (random.nextFloat() - 0.5f),
+                        0, // y is distal axis
+                        (random.nextFloat() - 0.5f)
+                );
+                offset.mul(skyDistance * 0.25f * (1 + j * .025f));
+                poseStack.pushPose();
+                poseStack.translate(offset.x, zoff, offset.z);
+                renderPlane(poseStack, tesselator, skyDistance * scale, 0, 1, GameRenderer::getPositionTexColorShader, WISP_LOCATION, clusterScale);
+                poseStack.popPose();
+//                poseStack.scale(0.75f, 0.75f, 0.75f);
+                zoff += 0.05f;
+            }
+            poseStack.popPose();
         }
         RenderSystem.enableDepthTest();
         RenderSystem.depthMask(true);
@@ -107,5 +142,19 @@ public class PocketDimensionEffects extends DimensionSpecialEffects {
             BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
             poseStack.popPose();
         }
+    }
+
+    private static void renderPlane(PoseStack poseStack, Tesselator tesselator, float skyDistance, float uvMin, float uvMax, Supplier<ShaderInstance> shaderSupplier, ResourceLocation texture, float scale) {
+        RenderSystem.setShader(shaderSupplier);
+        RenderSystem.setShaderTexture(0, texture);
+        poseStack.pushPose();
+        Matrix4f matrix4f = poseStack.last().pose();
+        BufferBuilder bufferbuilder = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR);
+        bufferbuilder.addVertex(matrix4f, -skyDistance * scale, -skyDistance, -skyDistance * scale).setUv(uvMin, uvMin).setColor(-14145496);
+        bufferbuilder.addVertex(matrix4f, -skyDistance * scale, -skyDistance, skyDistance * scale).setUv(uvMin, uvMax).setColor(-14145496);
+        bufferbuilder.addVertex(matrix4f, skyDistance * scale, -skyDistance, skyDistance * scale).setUv(uvMax, uvMax).setColor(-14145496);
+        bufferbuilder.addVertex(matrix4f, skyDistance * scale, -skyDistance, -skyDistance * scale).setUv(uvMax, uvMin).setColor(-14145496);
+        BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
+        poseStack.popPose();
     }
 }
