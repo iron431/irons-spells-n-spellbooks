0bc227d34310dc3af77d1efcd3dec6129db02698
iron431 <34083081+iron431@users.noreply.github.com>
2025-06-16 12:32:07 -0500

    aquifiers appearing to work

--- src/main/java/io/redspace/ironsspellbooks/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/NoiseChunkGeneratorMixin.java
@@ -1,15 +1,15 @@
 package io.redspace.ironsspellbooks.mixin;
 
-import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.worldgen.AquiferHelper;
 import io.redspace.ironsspellbooks.worldgen.IExtendedNoiseChunk;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseChunk;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.blending.Blender;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,33 +18,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(NoiseBasedChunkGenerator.class)
 public class NoiseChunkGeneratorMixin {
 
-    @Inject(method = "createNoiseChunk", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "createNoiseChunk", at = @At("RETURN"))
     void irons_spellbooks$detectAquifers(ChunkAccess chunk, StructureManager structureManager, Blender blender, RandomState random, CallbackInfoReturnable<NoiseChunk> cir) {
-        var registry = structureManager.registryAccess().registryOrThrow(Registries.STRUCTURE);
-//        if (AquiferHelper.marked) {
-//            ((IExtendedNoiseChunk) cir.getReturnValue()).setAquifierStatus(new IExtendedNoiseChunk.AquifierNuke());
-//        }
         IExtendedNoiseChunk noisechunk = (IExtendedNoiseChunk) cir.getReturnValue();
-        var reference = structureManager.startsForStructure(chunk.getPos(), structure -> true);
-        var starts = structureManager.startsForStructure(chunk.getPos(), structure ->
-                registry.getKey(structure).getNamespace().equals(IronsSpellbooks.MODID));
-        if (!starts.isEmpty() //todo: expensive as hell check?
+        var starts = structureManager.startsForStructure(chunk.getPos(), structure -> AquiferHelper.getOrCacheStructures(structureManager).contains(structure));
+        if (!starts.isEmpty()
         ) {
-            noisechunk.setAquifierStatus(new IExtendedNoiseChunk.AquifierNuke());
-//            for(var start : starts){
-//                for (var structurepiece : start.getPieces()){
-//                    if (structurepiece instanceof PoolElementStructurePiece poolelementstructurepiece) {
-//                        if (!(poolelementstructurepiece.getElement() instanceof IndividualTerrainStructurePoolElement ironElement)) {
-//                            IronsSpellbooks.LOGGER.info("eingisegn");
-//                        }
-//                    }
-//                }
-//            }
-            if(!AquiferHelper.marked){
-                IronsSpellbooks.LOGGER.info("ergsgsg");
-            }
+            BoundingBox[] boundingBoxes = starts.stream().flatMap(s -> s.getPieces().stream()).map(StructurePiece::getBoundingBox).toArray(BoundingBox[]::new);
+            noisechunk.irons_spellbooks$setAquifierStatus(new IExtendedNoiseChunk.AquifierNuke(boundingBoxes));
         }
-//        cir.setReturnValue();
-        AquiferHelper.marked = false;
     }
 }
