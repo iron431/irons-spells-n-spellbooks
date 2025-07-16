a6bf5e965ab3584d374e4dc4abdde6c5f7dbb5d6
iron431 <34083081+iron431@users.noreply.github.com>
2025-07-16 13:59:57 -0500

    update fire riptide texture, make burning dash render fullbright

--- src/main/java/io/redspace/ironsspellbooks/mixin/SpinAttackEffectLayerMixin.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/SpinAttackEffectLayerMixin.java
@@ -4,35 +4,44 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.player.ClientMagicData;
+import io.redspace.ironsspellbooks.player.SpinAttackType;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.layers.SpinAttackEffectLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.Objects;
 
 @Mixin(SpinAttackEffectLayer.class)
 public class SpinAttackEffectLayerMixin {
+    @Unique
     private static final ResourceLocation FIRE_TEXTURE = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/fire_riptide.png");
+    @Unique
+    private static boolean fullbright = false;
 
     @ModifyVariable(method = "render", at = @At("STORE"))
     public VertexConsumer selectSpinAttackTexture(VertexConsumer original, PoseStack poseStack, MultiBufferSource buffer, int p_117528_, LivingEntity livingEntity, float f1, float f2, float f3, float f4, float f5, float f6) {
-        switch (ClientMagicData.getSyncedSpellData(livingEntity).getSpinAttackType()) {
-            case FIRE:
-                return buffer.getBuffer(RenderType.entityCutoutNoCull(FIRE_TEXTURE));
-            default:
-                return original;
+        fullbright = false;
+        if (ClientMagicData.getSyncedSpellData(livingEntity).getSpinAttackType() == SpinAttackType.FIRE) {
+            fullbright = true;
+            return buffer.getBuffer(RenderType.entityCutoutNoCull(FIRE_TEXTURE));
         }
+        return original;
     }
 
-//    @ModifyArg(
-//            method = "render",
-//            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/geom/ModelPart;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;II)V"),
-//            index = 2
-//    )
-//    int modifyLight(int originalLight) {
-//                return LightTexture.FULL_BRIGHT;
-//    }
+    @ModifyArg(
+            method = "render",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/geom/ModelPart;render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;II)V"),
+            index = 2
+    )
+    int modifyLight(int originalLight) {
+        return fullbright ? LightTexture.FULL_BRIGHT : originalLight;
+    }
 }
