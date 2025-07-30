9974ec391a6c4db90fc9c4fdfda3d680d3102656
iron431 <34083081+iron431@users.noreply.github.com>
2025-07-29 19:28:31 -0500

    fix mana bar not respecting gui hotbar height

--- src/main/java/io/redspace/ironsspellbooks/gui/overlays/ManaBarOverlay.java
+++ src/main/java/io/redspace/ironsspellbooks/gui/overlays/ManaBarOverlay.java
@@ -119,7 +119,7 @@ public class ManaBarOverlay implements LayeredDraw.Layer {
         if (anchor == Anchor.Hunger)
             return screenHeight - (getAndIncrementRightHeight(gui) - 2) - IMAGE_HEIGHT / 2;
         if (anchor == Anchor.Center)
-            return screenHeight - HOTBAR_HEIGHT - (int) (ICON_ROW_HEIGHT * 2.5f) - IMAGE_HEIGHT / 2;
+            return screenHeight - HOTBAR_HEIGHT - (int) (ICON_ROW_HEIGHT * 2.5f) - IMAGE_HEIGHT / 2 - (Math.max(gui.rightHeight, gui.leftHeight) - 49);
         if (anchor == Anchor.TopLeft || anchor == Anchor.TopRight)
             return SCREEN_BORDER_MARGIN;
         return screenHeight - SCREEN_BORDER_MARGIN - IMAGE_HEIGHT;
