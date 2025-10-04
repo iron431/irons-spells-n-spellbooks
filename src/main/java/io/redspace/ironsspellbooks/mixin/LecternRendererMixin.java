1e5487c2d0722a955e31b84b8b6ad1728f9c7a42
iron431 <34083081+iron431@users.noreply.github.com>
2025-10-04 11:16:36 -0500

    port chronicle

--- src/main/java/io/redspace/ironsspellbooks/mixin/LecternRendererMixin.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/LecternRendererMixin.java
@@ -45,7 +45,7 @@ public class LecternRendererMixin {
                     this.bookModel.setupAnim(0.0F, 0.1F, 0.9F, 1.2F);
                     var vertexconsumer = pBufferSource.getBuffer(RenderType.entitySolid(textureOverride.get()));
                     this.bookModel.render(pPoseStack, vertexconsumer, pPackedLight, pPackedOverlay, 1f, 1f, 1f, 1f);
-                } else if (stack.getItem() instanceof SpellBook spellBook || stack.is(ItemRegistry.THE_CHRONICLE)/*todo: item tag for "3d lecterns"*/) {
+                } else if (stack.getItem() instanceof SpellBook spellBook || stack.is(ItemRegistry.THE_CHRONICLE.get())/*todo: item tag for "3d lecterns"*/) {
                     pPoseStack.mulPose(Axis.XP.rotationDegrees(-90f));
                     pPoseStack.mulPose(Axis.ZP.rotationDegrees(90f));
                     pPoseStack.mulPose(Axis.YP.rotationDegrees(180f));
