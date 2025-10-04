b4b20f069b72fa7cb8d20ee0f61ec38bdf818673
iron431 <34083081+iron431@users.noreply.github.com>
2025-10-04 11:52:35 -0500

    port chronicle 2

--- src/main/java/io/redspace/ironsspellbooks/api/backwards_compat/ClientHelper.java
+++ src/main/java/io/redspace/ironsspellbooks/api/backwards_compat/ClientHelper.java
@@ -1,5 +1,7 @@
 package io.redspace.ironsspellbooks.api.backwards_compat;
 
+import io.redspace.ironsspellbooks.gui.IronBookAccess;
+import io.redspace.ironsspellbooks.item.ILecternPlaceable;
 import io.redspace.ironsspellbooks.network.OpenHeldBookPacket;
 import io.redspace.ironsspellbooks.util.MinecraftInstanceHelper;
 import net.minecraft.client.Minecraft;
@@ -8,10 +10,14 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
 
 public class ClientHelper {
-    public static void handleOpenBookPacket(OpenHeldBookPacket packet){
+    public static void handleOpenBookPacket(OpenHeldBookPacket packet) {
         MinecraftInstanceHelper.ifPlayerPresent(player -> {
             ItemStack itemstack = player.getItemInHand(InteractionHand.values()[packet.hand]);
-            Minecraft.getInstance().setScreen(new BookViewScreen(new BookViewScreen.WrittenBookAccess(itemstack)));
+            if (itemstack.getItem() instanceof ILecternPlaceable iLecternPlaceable) {
+                Minecraft.getInstance().setScreen(new BookViewScreen(new IronBookAccess(iLecternPlaceable.getPages(itemstack))));
+            } else {
+                Minecraft.getInstance().setScreen(new BookViewScreen(new BookViewScreen.WrittenBookAccess(itemstack)));
+            }
         });
     }
 }
