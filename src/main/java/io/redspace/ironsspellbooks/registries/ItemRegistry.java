24a078a0e044a04b272a94f75cd9cfc482d8d673
iron431 <34083081+iron431@users.noreply.github.com>
2025-10-18 21:49:29 -0500

    disable katana item

--- src/main/java/io/redspace/ironsspellbooks/registries/ItemRegistry.java
+++ src/main/java/io/redspace/ironsspellbooks/registries/ItemRegistry.java
@@ -107,7 +107,8 @@ public class ItemRegistry {
             ItemPropertiesHelper.equipment().attributes(ExtendedSwordItem.createAttributes(ExtendedWeaponTier.TWILIGHT_GALE))
                     .rarity(Rarity.RARE)
                     .fireResistant(), SpellDataRegistryHolder.of(new SpellDataRegistryHolder(SpellRegistry.VOLT_STRIKE_SPELL, 5))));
-    public static final DeferredHolder<Item, Item> OBSIDIAN_KATANA_WIP = ITEMS.register("obsidian_katana", () -> new MagicSwordItem(ExtendedWeaponTier.SPELLBREAKER, ItemPropertiesHelper.equipment().rarity(Rarity.EPIC).attributes(ExtendedSwordItem.createAttributes(ExtendedWeaponTier.SPELLBREAKER)), SpellDataRegistryHolder.of(new SpellDataRegistryHolder(SpellRegistry.SHADOW_SLASH, 1))));
+//    public static final DeferredHolder<Item, Item> OBSIDIAN_KATANA_WIP = ITEMS.register("obsidian_katana",
+//            () -> new MagicSwordItem(ExtendedWeaponTier.VOID_ICHOR, ItemPropertiesHelper.equipment().rarity(Rarity.EPIC).attributes(ExtendedSwordItem.createAttributes(ExtendedWeaponTier.VOID_ICHOR)), SpellDataRegistryHolder.of(new SpellDataRegistryHolder(SpellRegistry.SHADOW_SLASH, 3))));
 
 
     /**
