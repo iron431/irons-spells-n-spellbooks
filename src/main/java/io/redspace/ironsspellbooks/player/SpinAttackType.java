12caf80ca3e1a04282afe797e8e5c655c97ccaf8
iron431 <34083081+iron431@users.noreply.github.com>
2026-01-05 18:35:42 -0600

    Update SpinAttackType.java

--- src/main/java/io/redspace/ironsspellbooks/player/SpinAttackType.java
+++ src/main/java/io/redspace/ironsspellbooks/player/SpinAttackType.java
@@ -1,5 +1,9 @@
 package io.redspace.ironsspellbooks.player;
 
+/**
+ * Enum is non-extensible and this construct should be replaced with objects or be string-represented
+ */
+@Deprecated(forRemoval = true)
 public enum SpinAttackType {
     FIRE,
     LIGHTNING,
