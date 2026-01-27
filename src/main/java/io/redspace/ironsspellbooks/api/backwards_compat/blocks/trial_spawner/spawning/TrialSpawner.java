5744a29b15adebd54aa13d5cdef99c9716bfdf18
iron431 <34083081+iron431@users.noreply.github.com>
2026-01-27 14:55:23 -0600

    fix trial spawner respecting light level

--- src/main/java/io/redspace/ironsspellbooks/api/backwards_compat/blocks/trial_spawner/spawning/TrialSpawner.java
+++ src/main/java/io/redspace/ironsspellbooks/api/backwards_compat/blocks/trial_spawner/spawning/TrialSpawner.java
@@ -201,7 +201,7 @@ public final class TrialSpawner {
                     return Optional.empty();
                 } else {
                     BlockPos blockpos = BlockPos.containing(vec3);
-                    if (!SpawnPlacements.checkSpawnRules(optional.get(), level, MobSpawnType.SPAWNER, blockpos, level.getRandom())) {
+                    if (!SpawnPlacements.checkSpawnRules(optional.get(), level, MobSpawnType.MOB_SUMMONED, blockpos, level.getRandom())) {
                         return Optional.empty();
                     } else {
                         if (spawndata.getCustomSpawnRules().isPresent()) {
