6cc4efbb881870c5ce6ad58e7aad5deb2bb05831
iron431 <34083081+iron431@users.noreply.github.com>
2026-01-30 13:12:08 -0600

    fix particle subscribing in server dist

--- src/main/java/io/redspace/ironsspellbooks/particle/FallingBlockParticle.java
+++ src/main/java/io/redspace/ironsspellbooks/particle/FallingBlockParticle.java
@@ -31,7 +31,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@EventBusSubscriber
+@EventBusSubscriber(Dist.CLIENT)
 public class FallingBlockParticle extends TextureSheetParticle {
     private final BlockState blockState;
     private final boolean particlesOnImpact;
