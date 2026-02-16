144d47dd3c3f943c3ed6626fd675bd11271f2329
iron431 <34083081+iron431@users.noreply.github.com>
2026-02-16 16:06:58 -0600

    fix cryomancer aggro
    addresses #1065

--- src/main/java/io/redspace/ironsspellbooks/entity/mobs/wizards/cryomancer/CryomancerEntity.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/mobs/wizards/cryomancer/CryomancerEntity.java
@@ -70,7 +70,7 @@ public class CryomancerEntity  extends NeutralWizard implements IMerchantWizard 
         //this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
         //this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isHostileTowards));
         this.targetSelector.addGoal(5, new ResetUniversalAngerTargetGoal<>(this, false));
     }
 
