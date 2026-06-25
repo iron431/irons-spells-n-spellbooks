e29c5f0904acca8ef4eea18513405e4b787bec4f
iron431 <34083081+iron431@users.noreply.github.com>
2026-06-25 12:08:04 -0500

    overhaul evasion teleport solver

--- src/main/java/io/redspace/ironsspellbooks/effect/EvasionEffect.java
+++ src/main/java/io/redspace/ironsspellbooks/effect/EvasionEffect.java
@@ -1,14 +1,16 @@
 package io.redspace.ironsspellbooks.effect;
 
 import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.util.Utils;
 import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.datagen.DamageTypeTagGenerator;
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.arguments.EntityAnchorArgument;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.DamageTypeTags;
@@ -17,7 +19,8 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class EvasionEffect extends CustomDescriptionMobEffect implements ISyncedMobEffect {
@@ -54,39 +57,32 @@ public class EvasionEffect extends CustomDescriptionMobEffect implements ISynced
         double d0 = livingEntity.getX();
         double d1 = livingEntity.getY();
         double d2 = livingEntity.getZ();
-        double maxRadius = 12d;
+        float maxRadius = 12;
         var level = livingEntity.level;
         var random = livingEntity.getRandom();
-
-        for (int i = 0; i < 16; ++i) {
-            var minRadius = maxRadius / 2;
-            Vec3 vec = new Vec3((double) random.nextInt((int) minRadius, (int) maxRadius), 0, 0);
-            int degrees = random.nextInt(360);
-            vec = vec.yRot(degrees * Mth.DEG_TO_RAD);
-
-            double x = d0 + vec.x;
-            double y = Mth.clamp(livingEntity.getY() + (double) (livingEntity.getRandom().nextInt((int) maxRadius) - maxRadius / 2), (double) level.getMinBuildHeight(), (double) (level.getMinBuildHeight() + ((ServerLevel) level).getLogicalHeight() - 1));
-            double z = d2 + vec.z;
-
-            if (livingEntity.isPassenger()) {
-                livingEntity.stopRiding();
-            }
-
-            if (livingEntity.randomTeleport(x, y, z, true)) {
+        Vec3 horizontalDirection = livingEntity.getForward().multiply(1, 0, 1).add(0.1,0,0).normalize();
+        Vec3 origin = livingEntity.getBoundingBox().getCenter();
+        float radius = maxRadius / 2f;
+        int range = 45;
+        for (int attempts = 0; attempts < 24; attempts++) {
+            Vec3 direction = horizontalDirection.scale(-1).yRot(random.nextIntBetweenInclusive(-range, range) * Mth.DEG_TO_RAD);
+            HitResult raycast = Utils.raycastForBlock(level, origin, origin.add(direction.scale(radius).add(0, 4, 0)), ClipContext.Fluid.NONE);
+            Vec3 destination = Utils.moveToRelativeGroundLevel(level, raycast.getLocation().subtract(direction.scale(livingEntity.getBbWidth())), 5);
+            if (destination.distanceToSqr(livingEntity.position()) > 4 && level.noCollision(livingEntity.getBoundingBox().move(destination.subtract(livingEntity.position())).inflate(-0.05)) && !level.getBlockState(BlockPos.containing(destination).below()).isAir()) {
+                particleCloud(livingEntity);
+                level.playSound(null, d0, d1, d2, SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
+                Utils.handleSpellTeleport(SpellRegistry.EVASION_SPELL.get(), livingEntity, destination);
                 if (damageSource.getEntity() != null) {
                     livingEntity.lookAt(EntityAnchorArgument.Anchor.EYES, damageSource.getEntity().getEyePosition());
                 }
-                level.playSound((Player) null, d0, d1, d2, SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
-                livingEntity.playSound(SoundEvents.ENDERMAN_TELEPORT, 2.0F, 1.0F);
                 break;
             }
-
-            if (maxRadius > 2) {
-                maxRadius--;
-            }
+            range *= 2;
+            radius += 1;
         }
-        //Vanilla teleport only spawns particles from the original location, not at the destination
+
         particleCloud(livingEntity);
+        level.playSound(null, livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
         return true;
     }
 
